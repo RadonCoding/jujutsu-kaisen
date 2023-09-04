43432275fd09212a21ef919d5883798c9f152965
RadonCoding <henry.welch@reborn.com>
2023-09-04 17:56:53 +0300

    bug fixes

--- src/main/java/radon/jujutsu_kaisen/command/SetTypeCommand.java
+++ src/main/java/radon/jujutsu_kaisen/command/SetTypeCommand.java
@@ -14,7 +14,7 @@ import radon.jujutsu_kaisen.network.packet.s2c.SyncSorcererDataS2CPacket;
 
 public class SetTypeCommand {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-        LiteralCommandNode<CommandSourceStack> node = dispatcher.register(Commands.literal("setcurse")
+        LiteralCommandNode<CommandSourceStack> node = dispatcher.register(Commands.literal("settype")
                 .requires((player) -> player.hasPermission(2))
                 .then(Commands.argument("player", EntityArgument.entity()).then(Commands.argument("type", EnumArgument.enumArgument(JujutsuType.class)).executes((ctx) ->
                         setType(EntityArgument.getPlayer(ctx, "player"), ctx.getArgument("type", JujutsuType.class))))));
