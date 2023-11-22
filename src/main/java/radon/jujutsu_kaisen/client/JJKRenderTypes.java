c9ede52a220cdc1b19023cfe9004da6039a0d705
RadonCoding <henry.welch@reborn.com>
2023-11-22 03:59:45 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/client/JJKRenderTypes.java
+++ src/main/java/radon/jujutsu_kaisen/client/JJKRenderTypes.java
@@ -30,7 +30,7 @@ public class JJKRenderTypes extends RenderType {
                 false, false, CompositeState.builder()
                         .setShaderState(RENDERTYPE_ENERGY_SWIRL_SHADER)
                         .setTextureState(shard)
-                        .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
+                        .setTransparencyState(ADDITIVE_TRANSPARENCY)
                         .setCullState(NO_CULL)
                         .setLightmapState(LIGHTMAP)
                         .setOverlayState(OVERLAY)
