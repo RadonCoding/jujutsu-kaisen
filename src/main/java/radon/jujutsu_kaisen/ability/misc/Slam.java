2513d0f76a06dbba877d37136cf7fabbd052a549
RadonCoding <henry.welch@reborn.com>
2023-11-23 22:50:16 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/ability/misc/Slam.java
+++ src/main/java/radon/jujutsu_kaisen/ability/misc/Slam.java
@@ -2,7 +2,9 @@ package radon.jujutsu_kaisen.ability.misc;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
@@ -25,6 +27,8 @@ import radon.jujutsu_kaisen.ability.MenuType;
 import radon.jujutsu_kaisen.capability.data.ISorcererData;
 import radon.jujutsu_kaisen.capability.data.SorcererDataHandler;
 import radon.jujutsu_kaisen.client.ClientWrapper;
+import radon.jujutsu_kaisen.client.particle.ParticleColors;
+import radon.jujutsu_kaisen.client.particle.TravelParticle;
 import radon.jujutsu_kaisen.util.HelperMethods;
 
 import java.util.*;
@@ -57,10 +61,12 @@ public class Slam extends Ability implements Ability.IChannelened {
         return ActivationType.CHANNELED;
     }
 
-    private @Nullable HitResult getTarget(LivingEntity owner) {
-        HitResult hit = HelperMethods.getLookAtHit(owner, RANGE);
-        if (hit.getType() == HitResult.Type.MISS) return null;
-        return hit;
+    private Vec3 getTarget(LivingEntity owner) {
+        Vec3 start = owner.getEyePosition();
+        Vec3 look = owner.getLookAngle();
+        Vec3 end = start.add(look.scale(RANGE));
+        HitResult result = HelperMethods.getHitResult(owner, start, end);
+        return result.getType() == HitResult.Type.MISS ? end : result.getLocation();
     }
 
     @Override
@@ -105,8 +111,12 @@ public class Slam extends Ability implements Ability.IChannelened {
     }
 
     public static void onHitGround(LivingEntity owner, float distance) {
-        owner.swing(InteractionHand.MAIN_HAND);
-        ExplosionHandler.spawn(owner.level().dimension(), owner.position(), Math.min(MAX_EXPLOSION, distance * TARGETS.get(owner.getUUID())), 5, owner,
+        if (!(owner.level() instanceof ServerLevel level)) return;
+
+        float radius = Math.min(MAX_EXPLOSION, distance * TARGETS.get(owner.getUUID()));
+
+        owner.swing(InteractionHand.MAIN_HAND, true);
+        ExplosionHandler.spawn(owner.level().dimension(), owner.position(), radius, 5, owner,
                 owner instanceof Player player ? owner.damageSources().playerAttack(player) : owner.damageSources().mobAttack(owner), false);
         TARGETS.remove(owner.getUUID());
     }
@@ -117,9 +127,7 @@ public class Slam extends Ability implements Ability.IChannelened {
 
         if (owner.level().isClientSide) return;
 
-        HitResult target = this.getTarget(owner);
-
-        if (target == null) return;
+        Vec3 target = this.getTarget(owner);
 
         Vec3 direction = new Vec3(0.0D, LAUNCH_POWER, 0.0D);
         owner.setDeltaMovement(owner.getDeltaMovement().add(direction));
@@ -130,7 +138,7 @@ public class Slam extends Ability implements Ability.IChannelened {
         ISorcererData cap = owner.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
 
         cap.delayTickEvent(() -> {
-            owner.setDeltaMovement(owner.getDeltaMovement().add(target.getLocation().subtract(owner.position()).normalize().scale(5.0D)));
+            owner.setDeltaMovement(owner.getDeltaMovement().add(target.subtract(owner.position()).normalize().scale(5.0D)));
             owner.hurtMarked = true;
         }, 20);
     }
