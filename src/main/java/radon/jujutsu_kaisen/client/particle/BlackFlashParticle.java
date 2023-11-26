823d6e49cf97061046f0132ee30be2f21818ec9e
RadonCoding <henry.welch@reborn.com>
2023-11-26 16:20:48 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/client/particle/BlackFlashParticle.java
+++ src/main/java/radon/jujutsu_kaisen/client/particle/BlackFlashParticle.java
@@ -6,28 +6,20 @@ import net.minecraft.client.particle.ParticleRenderType;
 import net.minecraft.client.particle.SpriteSet;
 import net.minecraft.client.particle.TextureSheetParticle;
 import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.util.Mth;
 import org.jetbrains.annotations.NotNull;
 
 public class BlackFlashParticle extends TextureSheetParticle {
     private final SpriteSet sprites;
 
-    protected BlackFlashParticle(ClientLevel pLevel, double pX, double pY, double pZ, double pXSpeed, double pYSpeed, double pZSpeed, SpriteSet pSprites) {
+    protected BlackFlashParticle(ClientLevel pLevel, double pX, double pY, double pZ, SpriteSet pSprites) {
         super(pLevel, pX, pY, pZ);
 
-        this.friction = 0.7F;
-        this.gravity = 0.5F;
-        this.xd *= 0.1F;
-        this.yd *= 0.1F;
-        this.zd *= 0.1F;
-        this.xd += pXSpeed * 0.4D;
-        this.yd += pYSpeed * 0.4D;
-        this.zd += pZSpeed * 0.4D;
+        this.lifetime = (int)(2.0F / (this.random.nextFloat() * 0.9F + 0.1F));
+
         this.rCol = ParticleColors.BLACK_FLASH.x();
         this.gCol = ParticleColors.BLACK_FLASH.y();
         this.bCol = ParticleColors.BLACK_FLASH.z();
-        this.quadSize *= 2.0F;
-        this.lifetime = Math.max((int) (12.0D / (Math.random() * 0.8D + 0.6D)), 1);
-        this.hasPhysics = false;
 
         this.sprites = pSprites;
         this.setSprite(this.sprites.get(this.random));
@@ -42,7 +34,22 @@ public class BlackFlashParticle extends TextureSheetParticle {
 
     @Override
     public @NotNull ParticleRenderType getRenderType() {
-        return JJKParticleRenderTypes.BLACK_FLASH;
+        return JJKParticleRenderTypes.ADDITIVE;
+    }
+
+    @Override
+    protected int getLightColor(float pPartialTick) {
+        float f = ((float) this.age + pPartialTick) / (float) this.lifetime;
+        f = Mth.clamp(f, 0.0F, 1.0F);
+        int i = super.getLightColor(pPartialTick);
+        int j = i & 255;
+        int k = i >> 16 & 255;
+        j += (int) (f * 15.0F * 16.0F);
+
+        if (j > 240) {
+            j = 240;
+        }
+        return j | k << 16;
     }
 
     public static class Provider implements ParticleProvider<SimpleParticleType> {
@@ -55,7 +62,7 @@ public class BlackFlashParticle extends TextureSheetParticle {
         @Override
         public BlackFlashParticle createParticle(@NotNull SimpleParticleType type, @NotNull ClientLevel level, double x, double y, double z,
                                                  double xSpeed, double ySpeed, double zSpeed) {
-            return new BlackFlashParticle(level, x, y, z, xSpeed, ySpeed, zSpeed, this.sprites);
+            return new BlackFlashParticle(level, x, y, z, this.sprites);
         }
     }
 }
