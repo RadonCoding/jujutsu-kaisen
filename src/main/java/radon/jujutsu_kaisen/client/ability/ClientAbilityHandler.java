602b07d43e20d27492103c1a1d8c9629daa54b34
RadonCoding <henry.welch@reborn.com>
2023-11-27 22:03:55 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/client/ability/ClientAbilityHandler.java
+++ src/main/java/radon/jujutsu_kaisen/client/ability/ClientAbilityHandler.java
@@ -167,8 +167,6 @@ public class ClientAbilityHandler {
                         mc.gui.setOverlayMessage(Component.translatable(String.format("ability.%s.fail.failure", JujutsuKaisen.MOD_ID)), false);
                 case DOMAIN_AMPLIFICATION ->
                         mc.gui.setOverlayMessage(Component.translatable(String.format("ability.%s.fail.domain_amplification", JujutsuKaisen.MOD_ID)), false);
-                case SIMPLE_DOMAIN ->
-                        mc.gui.setOverlayMessage(Component.translatable(String.format("ability.%s.fail.simple_domain", JujutsuKaisen.MOD_ID)), false);
             }
         });
         return status == Ability.Status.SUCCESS;
