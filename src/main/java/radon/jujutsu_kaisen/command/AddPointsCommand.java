b907a6c4208d7782fa31247d3fc5602722b66635
RadonCoding <henry.welch@reborn.com>
2023-12-10 06:54:11 +0200

    Read CHANGES.md

---
+++ src/main/java/radon/jujutsu_kaisen/command/AddPointsCommand.java
@@ -0,0 +1,31 @@
+package radon.jujutsu_kaisen.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.server.level.ServerPlayer;
+import radon.jujutsu_kaisen.capability.data.SorcererDataHandler;
+import radon.jujutsu_kaisen.network.PacketHandler;
+import radon.jujutsu_kaisen.network.packet.s2c.SyncSorcererDataS2CPacket;
+
+public class AddPointsCommand {
+    public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+        LiteralCommandNode<CommandSourceStack> node = dispatcher.register(Commands.literal("addpoints")
+                .requires((player) -> player.hasPermission(2))
+                .then(Commands.argument("player", EntityArgument.entity()).then(Commands.argument("points", IntegerArgumentType.integer())
+                        .executes(ctx -> addPoints(EntityArgument.getPlayer(ctx, "player"), IntegerArgumentType.getInteger(ctx, "points"))))));
+
+        dispatcher.register(Commands.literal("addpoints").requires((player) -> player.hasPermission(2)).redirect(node));
+    }
+
+    public static int addPoints(ServerPlayer player, int points) {
+        player.getCapability(SorcererDataHandler.INSTANCE).ifPresent(cap -> {
+            cap.addPoints(points);
+            PacketHandler.sendToClient(new SyncSorcererDataS2CPacket(cap.serializeNBT()), player);
+        });
+        return 1;
+    }
+}
