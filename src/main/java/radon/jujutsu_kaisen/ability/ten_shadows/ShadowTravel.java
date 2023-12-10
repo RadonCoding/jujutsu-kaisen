359f48421b668a08d1e8f1fa436772e3272968c7
RadonCoding <henry.welch@reborn.com>
2023-12-10 07:48:49 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/ability/ten_shadows/ShadowTravel.java
+++ src/main/java/radon/jujutsu_kaisen/ability/ten_shadows/ShadowTravel.java
@@ -16,6 +16,7 @@ import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 import radon.jujutsu_kaisen.ability.MenuType;
 import radon.jujutsu_kaisen.ability.base.Ability;
+import radon.jujutsu_kaisen.entity.base.TenShadowsSummon;
 import radon.jujutsu_kaisen.util.HelperMethods;
 
 public class ShadowTravel extends Ability {
@@ -42,48 +43,49 @@ public class ShadowTravel extends Ability {
         if (hit.getType() == HitResult.Type.BLOCK && (owner.level().getBlockState(((BlockHitResult) hit).getBlockPos().above()).canOcclude() ||
                 ((BlockHitResult) hit).getDirection() != Direction.UP)) return null;
 
-        if (owner.level().getMaxLocalRawBrightness(BlockPos.containing(hit.getLocation()), 0) == 15) return null;
-
-        return hit;
+        if (hit instanceof EntityHitResult entityHit && entityHit.getEntity() instanceof TenShadowsSummon ||
+                (owner.level().isNight() && owner.level().getLightEmission(BlockPos.containing(hit.getLocation())) == 0) ||
+                owner.level().getMaxLocalRawBrightness(BlockPos.containing(hit.getLocation()), 0) < 15) {
+            return hit;
+        }
+        return null;
     }
 
     @Override
     public void run(LivingEntity owner) {
+        owner.swing(InteractionHand.MAIN_HAND);
+
+        if (!(owner.level() instanceof ServerLevel level)) return;
+
         HitResult target = this.getTarget(owner);
 
         if (target != null) {
-            owner.swing(InteractionHand.MAIN_HAND);
-
             owner.level().playSound(null, owner.getX(), owner.getY(), owner.getZ(), SoundEvents.FISHING_BOBBER_SPLASH, SoundSource.MASTER, 1.0F, 1.0F);
 
-            if (owner.level() instanceof ServerLevel level) {
-                for (int i = 0; i < 16; i++) {
-                    for (int j = 0; j < owner.getBbHeight() * owner.getBbHeight(); j++) {
-                        level.sendParticles(ParticleTypes.SMOKE, owner.getX() + (owner.getBbWidth() * HelperMethods.RANDOM.nextGaussian() * 0.1F), owner.getY(),
-                                owner.getZ() + (owner.getBbWidth() * HelperMethods.RANDOM.nextGaussian() * 0.1F), 0,
-                                HelperMethods.RANDOM.nextGaussian() * 0.075D, HelperMethods.RANDOM.nextGaussian() * 0.25D, HelperMethods.RANDOM.nextGaussian() * 0.075D, 1.0D);
-                        level.sendParticles(ParticleTypes.LARGE_SMOKE, owner.getX() + (owner.getBbWidth() * HelperMethods.RANDOM.nextGaussian() * 0.1F), owner.getY(),
-                                owner.getZ() + (owner.getBbWidth() * HelperMethods.RANDOM.nextGaussian() * 0.1F), 0,
-                                HelperMethods.RANDOM.nextGaussian() * 0.075D, HelperMethods.RANDOM.nextGaussian() * 0.25D, HelperMethods.RANDOM.nextGaussian() * 0.075D, 1.0D);
-                    }
+            for (int i = 0; i < 16; i++) {
+                for (int j = 0; j < owner.getBbHeight() * owner.getBbHeight(); j++) {
+                    level.sendParticles(ParticleTypes.SMOKE, owner.getX() + (owner.getBbWidth() * HelperMethods.RANDOM.nextGaussian() * 0.1F), owner.getY(),
+                            owner.getZ() + (owner.getBbWidth() * HelperMethods.RANDOM.nextGaussian() * 0.1F), 0,
+                            HelperMethods.RANDOM.nextGaussian() * 0.075D, HelperMethods.RANDOM.nextGaussian() * 0.25D, HelperMethods.RANDOM.nextGaussian() * 0.075D, 1.0D);
+                    level.sendParticles(ParticleTypes.LARGE_SMOKE, owner.getX() + (owner.getBbWidth() * HelperMethods.RANDOM.nextGaussian() * 0.1F), owner.getY(),
+                            owner.getZ() + (owner.getBbWidth() * HelperMethods.RANDOM.nextGaussian() * 0.1F), 0,
+                            HelperMethods.RANDOM.nextGaussian() * 0.075D, HelperMethods.RANDOM.nextGaussian() * 0.25D, HelperMethods.RANDOM.nextGaussian() * 0.075D, 1.0D);
                 }
             }
 
             Vec3 pos = target.getLocation();
-            owner.setPos(pos.x(), pos.y(), pos.z());
+            owner.teleportTo(pos.x(), pos.y(), pos.z());
 
             owner.level().playSound(null, owner.getX(), owner.getY(), owner.getZ(), SoundEvents.FISHING_BOBBER_SPLASH, SoundSource.MASTER, 1.0F, 1.0F);
 
-            if (owner.level() instanceof ServerLevel level) {
-                for (int i = 0; i < 16; i++) {
-                    for (int j = 0; j < owner.getBbHeight() * owner.getBbHeight(); j++) {
-                        level.sendParticles(ParticleTypes.SMOKE, owner.getX() + (owner.getBbWidth() * HelperMethods.RANDOM.nextGaussian() * 0.1F), owner.getY(),
-                                owner.getZ() + (owner.getBbWidth() * HelperMethods.RANDOM.nextGaussian() * 0.1F), 0,
-                                HelperMethods.RANDOM.nextGaussian() * 0.075D, HelperMethods.RANDOM.nextGaussian() * 0.25D, HelperMethods.RANDOM.nextGaussian() * 0.075D, 1.0D);
-                        level.sendParticles(ParticleTypes.LARGE_SMOKE, owner.getX() + (owner.getBbWidth() * HelperMethods.RANDOM.nextGaussian() * 0.1F), owner.getY(),
-                                owner.getZ() + (owner.getBbWidth() * HelperMethods.RANDOM.nextGaussian() * 0.1F), 0,
-                                HelperMethods.RANDOM.nextGaussian() * 0.075D, HelperMethods.RANDOM.nextGaussian() * 0.25D, HelperMethods.RANDOM.nextGaussian() * 0.075D, 1.0D);
-                    }
+            for (int i = 0; i < 16; i++) {
+                for (int j = 0; j < owner.getBbHeight() * owner.getBbHeight(); j++) {
+                    level.sendParticles(ParticleTypes.SMOKE, owner.getX() + (owner.getBbWidth() * HelperMethods.RANDOM.nextGaussian() * 0.1F), owner.getY(),
+                            owner.getZ() + (owner.getBbWidth() * HelperMethods.RANDOM.nextGaussian() * 0.1F), 0,
+                            HelperMethods.RANDOM.nextGaussian() * 0.075D, HelperMethods.RANDOM.nextGaussian() * 0.25D, HelperMethods.RANDOM.nextGaussian() * 0.075D, 1.0D);
+                    level.sendParticles(ParticleTypes.LARGE_SMOKE, owner.getX() + (owner.getBbWidth() * HelperMethods.RANDOM.nextGaussian() * 0.1F), owner.getY(),
+                            owner.getZ() + (owner.getBbWidth() * HelperMethods.RANDOM.nextGaussian() * 0.1F), 0,
+                            HelperMethods.RANDOM.nextGaussian() * 0.075D, HelperMethods.RANDOM.nextGaussian() * 0.25D, HelperMethods.RANDOM.nextGaussian() * 0.075D, 1.0D);
                 }
             }
         }
