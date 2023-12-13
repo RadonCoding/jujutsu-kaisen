ccbe554965b705d95e83f83fb7ed58a35c6b89b6
RadonCoding <henry.welch@reborn.com>
2023-12-13 15:55:33 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/entity/JJKEntities.java
+++ src/main/java/radon/jujutsu_kaisen/entity/JJKEntities.java
@@ -158,10 +158,6 @@ public class JJKEntities {
                     .sized(1.0F, 2.9F)
                     .build(new ResourceLocation(JujutsuKaisen.MOD_ID, "heian_sukuna")
                             .toString()));
-    public static RegistryObject<EntityType<CloneEntity>> CLONE = ENTITIES.register("clone", () ->
-            EntityType.Builder.<CloneEntity>of(CloneEntity::new, MobCategory.MISC)
-                    .build(new ResourceLocation(JujutsuKaisen.MOD_ID, "clone")
-                            .toString()));
     public static RegistryObject<EntityType<NaoyaZeninEntity>> NAOYA_ZENIN = ENTITIES.register("naoya_zenin", () ->
             EntityType.Builder.<NaoyaZeninEntity>of(NaoyaZeninEntity::new, MobCategory.MISC)
                     .build(new ResourceLocation(JujutsuKaisen.MOD_ID, "naoya_zenin")
@@ -433,7 +429,6 @@ public class JJKEntities {
         event.put(TOGE_INUMAKI.get(), SorcererEntity.createAttributes().build());
         event.put(SUGURU_GETO.get(), SorcererEntity.createAttributes().build());
         event.put(HEIAN_SUKUNA.get(), SorcererEntity.createAttributes().build());
-        event.put(CLONE.get(), SorcererEntity.createAttributes().build());
         event.put(NAOYA_ZENIN.get(), SorcererEntity.createAttributes().build());
 
         event.put(RIKA.get(), RikaEntity.createAttributes().build());
--- src/main/java/radon/jujutsu_kaisen/event/PactEventHandler.java
+++ src/main/java/radon/jujutsu_kaisen/event/PactEventHandler.java
@@ -6,8 +6,8 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
-import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import radon.jujutsu_kaisen.JujutsuKaisen;
@@ -15,10 +15,7 @@ import radon.jujutsu_kaisen.capability.data.ISorcererData;
 import radon.jujutsu_kaisen.capability.data.SorcererDataHandler;
 import radon.jujutsu_kaisen.capability.data.sorcerer.BindingVow;
 import radon.jujutsu_kaisen.capability.data.sorcerer.Pact;
-import radon.jujutsu_kaisen.client.particle.CursedEnergyParticle;
-import radon.jujutsu_kaisen.client.particle.ParticleColors;
 import radon.jujutsu_kaisen.damage.JJKDamageSources;
-import radon.jujutsu_kaisen.entity.CloneEntity;
 import radon.jujutsu_kaisen.network.PacketHandler;
 import radon.jujutsu_kaisen.network.packet.s2c.SyncSorcererDataS2CPacket;
 import radon.jujutsu_kaisen.util.HelperMethods;
@@ -50,6 +47,17 @@ public class PactEventHandler {
                     }
                 }
             }
+        }
+
+        @SubscribeEvent
+        public static void onLivingAttack(LivingAttackEvent event) {
+            LivingEntity victim = event.getEntity();
+
+            if (victim.level().isClientSide) return;
+
+            DamageSource source = event.getSource();
+
+            if (!(source.getEntity() instanceof LivingEntity attacker)) return;
 
             // Check for Pact.INVULNERABILITY
             if (victim.getCapability(SorcererDataHandler.INSTANCE).isPresent() && attacker.getCapability(SorcererDataHandler.INSTANCE).isPresent()) {
@@ -57,23 +65,7 @@ public class PactEventHandler {
                 ISorcererData attackerCap = attacker.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
 
                 if (victimCap.hasPact(attacker.getUUID(), Pact.INVULNERABILITY) && attackerCap.hasPact(victim.getUUID(), Pact.INVULNERABILITY)) {
-                    victimCap.removePact(attacker.getUUID(), Pact.INVULNERABILITY);
-                    attackerCap.removePact(victim.getUUID(), Pact.INVULNERABILITY);
-
-                    MinecraftServer server = attacker.level().getServer();
-
-                    if (server != null) {
-                        ServerLevel dimension = server.getLevel(JJKDimensions.LIMBO_KEY);
-
-                        if (dimension != null) {
-                            BlockPos pos = HelperMethods.findSafePos(dimension, attacker);
-                            attacker.teleportTo(dimension, pos.getX(), pos.getY(), pos.getZ(), Set.of(), attacker.getYRot(), attacker.getXRot());
-
-                            attacker.level().addFreshEntity(new CloneEntity(attacker, attacker.level().dimension().location()));
-                        }
-                    }
-                    PacketHandler.sendToClient(new SyncSorcererDataS2CPacket(victimCap.serializeNBT()), (ServerPlayer) victim);
-                    PacketHandler.sendToClient(new SyncSorcererDataS2CPacket(attackerCap.serializeNBT()), (ServerPlayer) attacker);
+                    event.setCanceled(true);
                 }
             }
         }
