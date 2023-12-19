d097db190a40deca44b0a57a5e55ece5eeffc158
RadonCoding <henry.welch@reborn.com>
2023-12-19 21:15:58 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/entity/projectile/HollowPurpleProjectile.java
+++ src/main/java/radon/jujutsu_kaisen/entity/projectile/HollowPurpleProjectile.java
@@ -60,7 +60,6 @@ public class HollowPurpleProjectile extends JujutsuProjectile {
 
         if (this.getOwner() instanceof LivingEntity owner) {
             for (Entity entity : this.level().getEntities(owner, bounds)) {
-                if (!(entity instanceof LivingEntity living) || !owner.canAttack(living)) continue;
                 entity.hurt(JJKDamageSources.indirectJujutsuAttack(this, owner, JJKAbilities.HOLLOW_PURPLE.get()),
                         DAMAGE * this.getPower());
             }
