9d695465f9d611f68ca062ad3a8106f8660dd3ff
RadonCoding <henry.welch@reborn.com>
2023-12-21 00:38:27 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/client/JJKClientEventHandler.java
+++ src/main/java/radon/jujutsu_kaisen/client/JJKClientEventHandler.java
@@ -391,11 +391,6 @@ public class JJKClientEventHandler {
             event.registerLayerDefinition(NaoyaZeninModel.INNER_LAYER, SkinModel::createInnerLayer);
             event.registerLayerDefinition(NaoyaZeninModel.OUTER_LAYER, SkinModel::createOuterLayer);
 
-            event.registerLayerDefinition(SukunaRenderer.LAYER, SkinModel::createBodyLayer);
-            event.registerLayerDefinition(SukunaRenderer.LAYER_SLIM, SkinModel::createSlimBodyLayer);
-            event.registerLayerDefinition(SukunaRenderer.INNER_LAYER, SkinModel::createInnerLayer);
-            event.registerLayerDefinition(SukunaRenderer.OUTER_LAYER, SkinModel::createOuterLayer);
-
             event.registerLayerDefinition(InstantSpiritBodyOfDistortedKillingModel.LAYER_LOCATION, InstantSpiritBodyOfDistortedKillingModel::createBodyLayer);
         }
 
