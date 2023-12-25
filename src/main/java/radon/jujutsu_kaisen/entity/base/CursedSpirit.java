f0fac02fd31bd11752db1a076affdbc82229bfdc
RadonCoding <henry.welch@reborn.com>
2023-12-25 17:34:36 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/entity/base/CursedSpirit.java
+++ src/main/java/radon/jujutsu_kaisen/entity/base/CursedSpirit.java
@@ -5,6 +5,9 @@ import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
@@ -275,5 +278,22 @@ public abstract class CursedSpirit extends TamableAnimal implements GeoEntity, I
     @Override
     public void changeTarget(LivingEntity target) {
         this.setTarget(target);
+    }
+
+    @Override
+    public @NotNull Packet<ClientGamePacketListener> getAddEntityPacket() {
+        Entity entity = this.getOwner();
+        return new ClientboundAddEntityPacket(this, entity == null ? 0 : entity.getId());
+    }
+
+    @Override
+    public void recreateFromPacket(@NotNull ClientboundAddEntityPacket pPacket) {
+        super.recreateFromPacket(pPacket);
+
+        LivingEntity owner = (LivingEntity) this.level().getEntity(pPacket.getData());
+
+        if (owner != null) {
+            this.setOwner(owner);
+        }
     }
 }
