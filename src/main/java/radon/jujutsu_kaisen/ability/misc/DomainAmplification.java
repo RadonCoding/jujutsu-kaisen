2402dcee0def1ad7ff56cbf21468b8fa4409e9e8
RadonCoding <henry.welch@reborn.com>
2023-12-26 19:49:08 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/ability/misc/DomainAmplification.java
+++ src/main/java/radon/jujutsu_kaisen/ability/misc/DomainAmplification.java
@@ -36,7 +36,8 @@ public class DomainAmplification extends Ability implements Ability.IToggled {
 
         if (!HelperMethods.isExperienced(cap.getExperience()) && JJKAbilities.hasToggled(owner, domain)) return false;
 
-        if (!JJKAbilities.hasToggled(owner, JJKAbilities.INFINITY.get()) && (owner.level().getEntitiesOfClass(Projectile.class, owner.getBoundingBox().inflate(1.0D)).stream().anyMatch(entity -> entity.getOwner() != owner) {
+        if (!JJKAbilities.hasToggled(owner, JJKAbilities.INFINITY.get()) && (owner.level().getEntitiesOfClass(Projectile.class,
+                owner.getBoundingBox().inflate(1.0D)).stream().anyMatch(entity -> entity.getOwner() != owner))) {
             return true;
         }
         return target != null && JJKAbilities.hasToggled(target, JJKAbilities.INFINITY.get()) && owner.distanceTo(target) <= 3.0D;
