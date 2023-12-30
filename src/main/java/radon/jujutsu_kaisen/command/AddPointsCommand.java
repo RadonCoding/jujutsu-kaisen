0718e59a3f158afb32420bb9022fc31ab0c0208c
RadonCoding <henry.welch@reborn.com>
2023-12-30 13:45:24 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/command/AddPointsCommand.java
+++ src/main/java/radon/jujutsu_kaisen/command/AddPointsCommand.java
@@ -7,6 +7,7 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.server.level.ServerPlayer;
+import radon.jujutsu_kaisen.capability.data.ISorcererData;
 import radon.jujutsu_kaisen.capability.data.SorcererDataHandler;
 import radon.jujutsu_kaisen.network.PacketHandler;
 import radon.jujutsu_kaisen.network.packet.s2c.SyncSorcererDataS2CPacket;
@@ -22,10 +23,9 @@ public class AddPointsCommand {
     }
 
     public static int addPoints(ServerPlayer player, int points) {
-        player.getCapability(SorcererDataHandler.INSTANCE).ifPresent(cap -> {
-            cap.addPoints(points);
-            PacketHandler.sendToClient(new SyncSorcererDataS2CPacket(cap.serializeNBT()), player);
-        });
+        ISorcererData cap = player.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
+        cap.addPoints(points);
+        PacketHandler.sendToClient(new SyncSorcererDataS2CPacket(cap.serializeNBT()), player);
         return 1;
     }
 }
--- src/main/java/radon/jujutsu_kaisen/command/SetTypeCommand.java
+++ src/main/java/radon/jujutsu_kaisen/command/SetTypeCommand.java
@@ -7,6 +7,7 @@ import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.server.command.EnumArgument;
+import radon.jujutsu_kaisen.capability.data.ISorcererData;
 import radon.jujutsu_kaisen.capability.data.SorcererDataHandler;
 import radon.jujutsu_kaisen.capability.data.sorcerer.JujutsuType;
 import radon.jujutsu_kaisen.network.PacketHandler;
@@ -25,10 +26,9 @@ public class SetTypeCommand {
     public static int setType(ServerPlayer player, JujutsuType type) {
         if (type == JujutsuType.SHIKIGAMI) return 0;
 
-        player.getCapability(SorcererDataHandler.INSTANCE).ifPresent(cap -> {
-            cap.setType(type);
-            PacketHandler.sendToClient(new SyncSorcererDataS2CPacket(cap.serializeNBT()), player);
-        });
+        ISorcererData cap = player.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
+        cap.setType(type);
+        PacketHandler.sendToClient(new SyncSorcererDataS2CPacket(cap.serializeNBT()), player);
         return 1;
     }
 }
