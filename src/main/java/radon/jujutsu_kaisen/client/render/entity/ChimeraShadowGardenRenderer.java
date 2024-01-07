d66d0bdecdd1188db0d6e560f80198ecf9ace2e9
RadonCoding <henry.welch@reborn.com>
2024-01-07 13:20:09 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/client/render/entity/ChimeraShadowGardenRenderer.java
+++ src/main/java/radon/jujutsu_kaisen/client/render/entity/ChimeraShadowGardenRenderer.java
@@ -1,14 +1,31 @@
 package radon.jujutsu_kaisen.client.render.entity;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import radon.jujutsu_kaisen.JujutsuKaisen;
 import radon.jujutsu_kaisen.entity.ChimeraShadowGardenEntity;
+import radon.jujutsu_kaisen.entity.MalevolentShrineEntity;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.model.DefaultedEntityGeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
 public class ChimeraShadowGardenRenderer extends GeoEntityRenderer<ChimeraShadowGardenEntity> {
     public ChimeraShadowGardenRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new DefaultedEntityGeoModel<>(new ResourceLocation(JujutsuKaisen.MOD_ID, "chimera_shadow_garden")));
+    }
+
+    @Override
+    public void preRender(PoseStack poseStack, ChimeraShadowGardenEntity animatable, BakedGeoModel model, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        float yaw = Mth.lerp(partialTick, animatable.yRotO, animatable.getYRot());
+        float pitch = Mth.lerp(partialTick, animatable.xRotO, animatable.getXRot());
+
+        poseStack.mulPose(Axis.YP.rotationDegrees(yaw));
+
+        super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
     }
 }
