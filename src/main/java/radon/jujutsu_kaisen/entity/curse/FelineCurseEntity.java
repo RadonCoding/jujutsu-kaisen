ab70e9be5c79277517873acf70307102479c340c
RadonCoding <henry.welch@reborn.com>
2024-01-11 13:30:59 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/entity/curse/FelineCurseEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/curse/FelineCurseEntity.java
@@ -3,9 +3,10 @@ package radon.jujutsu_kaisen.entity.curse;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.LeapAtTargetGoal;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
@@ -13,7 +14,11 @@ import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.IronGolem;
 import net.minecraft.world.entity.npc.AbstractVillager;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import radon.jujutsu_kaisen.ability.base.Ability;
 import radon.jujutsu_kaisen.capability.data.sorcerer.CursedTechnique;
@@ -45,6 +50,64 @@ public class FelineCurseEntity extends PackCursedSpirit {
         this(JJKEntities.FELINE_CURSE.get(), leader.level());
 
         this.setLeader(leader);
+    }
+
+    @Override
+    public @NotNull InteractionResult mobInteract(@NotNull Player pPlayer, @NotNull InteractionHand pHand) {
+        if (pPlayer == this.getOwner() && !this.isVehicle()) {
+            if (pPlayer.startRiding(this)) {
+                pPlayer.setYRot(this.getYRot());
+                pPlayer.setXRot(this.getXRot());
+            }
+            return InteractionResult.sidedSuccess(this.level().isClientSide);
+        } else {
+            return super.mobInteract(pPlayer, pHand);
+        }
+    }
+
+    @Nullable
+    @Override
+    public LivingEntity getControllingPassenger() {
+        Entity entity = this.getFirstPassenger();
+
+        if (entity instanceof LivingEntity living) {
+            return living;
+        }
+        return null;
+    }
+
+    private Vec2 getRiddenRotation(LivingEntity pEntity) {
+        return new Vec2(pEntity.getXRot() * 0.5F, pEntity.getYRot());
+    }
+
+    @Override
+    protected float getRiddenSpeed(@NotNull Player pPlayer) {
+        return (float) this.getAttributeValue(Attributes.MOVEMENT_SPEED) * 2.0F;
+    }
+
+    @Override
+    protected void tickRidden(@NotNull Player pPlayer, @NotNull Vec3 pTravelVector) {
+        super.tickRidden(pPlayer, pTravelVector);
+
+        Vec2 vec2 = this.getRiddenRotation(pPlayer);
+        this.setRot(vec2.y, vec2.x);
+        this.yRotO = this.yBodyRot = this.yHeadRot = this.yHeadRotO = this.getYRot();
+    }
+
+    @Override
+    protected @NotNull Vec3 getRiddenInput(@NotNull Player pPlayer, @NotNull Vec3 pTravelVector) {
+        float f = pPlayer.xxa * 0.5F;
+        float f1 = pPlayer.zza;
+
+        if (f1 <= 0.0F) {
+            f1 *= 0.25F;
+        }
+        return new Vec3(f, 0.0D, f1);
+    }
+
+    @Override
+    public float getStepHeight() {
+        return 1.0F;
     }
 
     @Override
@@ -153,11 +216,6 @@ public class FelineCurseEntity extends PackCursedSpirit {
         if (leap > 0) {
             this.entityData.set(DATA_LEAP, --leap);
         }
-    }
-
-    @Override
-    public float getStepHeight() {
-        return 1.0F;
     }
 
     private class CustomLeapAtTargetGoal extends LeapAtTargetGoal {
