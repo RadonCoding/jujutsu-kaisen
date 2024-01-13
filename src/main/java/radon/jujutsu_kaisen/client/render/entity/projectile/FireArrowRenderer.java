b6ee9cacfafdad6b29e577efe7fd42484a537036
RadonCoding <henry.welch@reborn.com>
2024-01-13 14:09:48 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/client/render/entity/projectile/FireArrowRenderer.java
+++ src/main/java/radon/jujutsu_kaisen/client/render/entity/projectile/FireArrowRenderer.java
@@ -40,9 +40,8 @@ public class FireArrowRenderer extends EntityRenderer<FireArrowProjectile> {
         float yaw = Mth.lerp(pPartialTick, pEntity.yRotO, pEntity.getYRot());
         float pitch = Mth.lerp(pPartialTick, pEntity.xRotO, pEntity.getXRot());
 
-        pPoseStack.mulPose(Axis.YP.rotationDegrees(yaw + 180.0F));
+        pPoseStack.mulPose(Axis.YP.rotationDegrees(yaw + 90.0F));
         pPoseStack.mulPose(Axis.ZP.rotationDegrees(pitch));
-        pPoseStack.mulPose(Axis.XP.rotationDegrees(45.0F));
 
         pPoseStack.scale(1.5F, 1.5F, 1.5F);
 
