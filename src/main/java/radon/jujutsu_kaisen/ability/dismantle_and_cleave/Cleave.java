7a8b3d41e383f69cc124d1fe9ce407f0f3c31664
RadonCoding <henry.welch@reborn.com>
2024-01-15 01:06:19 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/ability/dismantle_and_cleave/Cleave.java
+++ src/main/java/radon/jujutsu_kaisen/ability/dismantle_and_cleave/Cleave.java
@@ -109,6 +109,12 @@ public class Cleave extends Ability implements Ability.IDomainAttack, Ability.IA
                 if (!target.isDeadOrDying()) {
                     level.sendParticles(JJKParticles.SLASH.get(), target.getX(), target.getY(), target.getZ(), 0, target.getId(),
                             0.0D, 0.0D, 1.0D);
+
+                    Vec3 center = target.position().add(0.0D, target.getBbHeight() / 2.0F, 0.0D);
+                    Vec3 offset = center.add((HelperMethods.RANDOM.nextDouble() - 0.5D) * target.getBbWidth(),
+                            (HelperMethods.RANDOM.nextDouble() - 0.5D) * target.getBbHeight(),
+                            (HelperMethods.RANDOM.nextDouble() - 0.5D) * target.getBbWidth());
+                    ((ServerLevel) owner.level()).sendParticles(ParticleTypes.EXPLOSION, offset.x, offset.y, offset.z, 0, 1.0D, 0.0D, 0.0D, 1.0D);
                 }
             }, i);
         }
