2dc9f0ab95247c4e25123d34212f3e1dde18964a
RadonCoding <henry.welch@reborn.com>
2024-01-21 17:38:57 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/client/render/entity/projectile/CursedBudRenderer.java
+++ src/main/java/radon/jujutsu_kaisen/client/render/entity/projectile/CursedBudRenderer.java
@@ -26,7 +26,7 @@ public class CursedBudRenderer extends GeoEntityRenderer<CursedBudProjectile> {
         float pitch = Mth.lerp(partialTick, animatable.xRotO, animatable.getXRot());
 
         poseStack.mulPose(Axis.YP.rotationDegrees(yaw));
-        poseStack.mulPose(Axis.XP.rotationDegrees(pitch));
+        poseStack.mulPose(Axis.XN.rotationDegrees(pitch));
 
         super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
     }
