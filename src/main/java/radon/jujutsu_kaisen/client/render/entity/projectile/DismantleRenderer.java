604f9b68dbcda30d526a242d1172da2902ae25e3
RadonCoding <henry.welch@reborn.com>
2024-01-21 17:21:15 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/client/render/entity/projectile/DismantleRenderer.java
+++ src/main/java/radon/jujutsu_kaisen/client/render/entity/projectile/DismantleRenderer.java
@@ -37,7 +37,7 @@ public class DismantleRenderer extends EntityRenderer<DismantleProjectile> {
         float pitch = Mth.lerp(pPartialTick, pEntity.xRotO, pEntity.getXRot());
 
         pPoseStack.mulPose(Axis.YP.rotationDegrees(yaw));
-        pPoseStack.mulPose(Axis.XP.rotationDegrees(pitch - 90.0F));
+        pPoseStack.mulPose(Axis.XN.rotationDegrees(pitch));
 
         pPoseStack.mulPose(Axis.YP.rotationDegrees(pEntity.getRoll()));
 
