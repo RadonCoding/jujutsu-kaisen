49d8e8b26c6e3944bf8423d20f3deac09b351584
RadonCoding <henry.welch@reborn.com>
2024-01-22 19:04:55 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/MahoragaEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/MahoragaEntity.java
@@ -105,7 +105,7 @@ public class MahoragaEntity extends TenShadowsSummon {
 
     public static AttributeSupplier.Builder createAttributes() {
         return SorcererEntity.createAttributes()
-                .add(Attributes.MAX_HEALTH, 5 * 20.0D)
+                .add(Attributes.MAX_HEALTH, 10 * 20.0D)
                 .add(Attributes.ATTACK_DAMAGE, 5 * 2.0D);
     }
 
