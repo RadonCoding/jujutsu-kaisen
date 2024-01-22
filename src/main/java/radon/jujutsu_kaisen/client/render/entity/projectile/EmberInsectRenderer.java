bd8cb3bf31bb54ae5604c8783697f6a66d5966d9
RadonCoding <henry.welch@reborn.com>
2024-01-23 01:16:51 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/client/render/entity/projectile/EmberInsectRenderer.java
+++ src/main/java/radon/jujutsu_kaisen/client/render/entity/projectile/EmberInsectRenderer.java
@@ -27,8 +27,8 @@ public class EmberInsectRenderer<T extends Entity & GeoEntity> extends GeoEntity
         float yaw = Mth.lerp(partialTick, animatable.yRotO, animatable.getYRot());
         float pitch = Mth.lerp(partialTick, animatable.xRotO, animatable.getXRot());
 
-        poseStack.mulPose(Axis.YP.rotationDegrees(360.0F - yaw));
-        poseStack.mulPose(Axis.XP.rotationDegrees(pitch));
+        poseStack.mulPose(Axis.YP.rotationDegrees(yaw));
+        poseStack.mulPose(Axis.XN.rotationDegrees(pitch));
 
         super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
     }
