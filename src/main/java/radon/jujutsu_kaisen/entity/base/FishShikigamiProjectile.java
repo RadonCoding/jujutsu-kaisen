80a411e881232590ec6c8d68ad00997b3552ae24
RadonCoding <henry.welch@reborn.com>
2024-01-24 23:32:34 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/entity/base/FishShikigamiProjectile.java
+++ src/main/java/radon/jujutsu_kaisen/entity/base/FishShikigamiProjectile.java
@@ -162,18 +162,18 @@ public class FishShikigamiProjectile extends JujutsuProjectile implements GeoEnt
 
         Entity entity = pResult.getEntity();
 
-        if (this.getOwner() instanceof LivingEntity owner) {
-            if (entity == owner) return;
+        if (!(this.getOwner() instanceof LivingEntity owner)) return;
 
-            if (this.isDomain()) {
-                ISorcererData cap = owner.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
-                DomainExpansionEntity domain = cap.getSummonByClass(DomainExpansionEntity.class);
-                entity.hurt(JJKDamageSources.indirectJujutsuAttack(domain == null ? this : domain, owner, null), DAMAGE * this.getPower());
-            } else {
-                entity.hurt(JJKDamageSources.indirectJujutsuAttack(this, owner, JJKAbilities.DEATH_SWARM.get()), DAMAGE * this.getPower());
-            }
-            this.discard();
+        if (entity != this.getTarget()) return;
+
+        if (this.isDomain()) {
+            ISorcererData cap = owner.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
+            DomainExpansionEntity domain = cap.getSummonByClass(DomainExpansionEntity.class);
+            entity.hurt(JJKDamageSources.indirectJujutsuAttack(domain == null ? this : domain, owner, null), DAMAGE * this.getPower());
+        } else {
+            entity.hurt(JJKDamageSources.indirectJujutsuAttack(this, owner, JJKAbilities.DEATH_SWARM.get()), DAMAGE * this.getPower());
         }
+        this.discard();
     }
 
     @Override
