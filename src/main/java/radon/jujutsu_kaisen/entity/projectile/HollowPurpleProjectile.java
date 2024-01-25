a63b9efcd8d83954c4766d23d7def72987aa8d43
codeblad <152137808+codeblad@users.noreply.github.com>
2024-01-25 16:45:13 -0500

    hollow purple damage and startup

--- src/main/java/radon/jujutsu_kaisen/entity/projectile/HollowPurpleProjectile.java
+++ src/main/java/radon/jujutsu_kaisen/entity/projectile/HollowPurpleProjectile.java
@@ -25,10 +25,10 @@ import radon.jujutsu_kaisen.util.HelperMethods;
 import radon.jujutsu_kaisen.util.RotationUtil;
 
 public class HollowPurpleProjectile extends JujutsuProjectile {
-    private static final int DELAY = 2 * 20;
+    private static final int DELAY = 50;//2 * 20;
     private static final float SPEED = 5.0F;
     private static final int DURATION = 5 * 20;
-    private static final float DAMAGE = 30.0F;
+    private static final float DAMAGE = 60.0F;
     private static final float MAX_RADIUS = 15.0F;
     private static final float RADIUS = 1.0F;
     private static final int ANIMATION = 20;
