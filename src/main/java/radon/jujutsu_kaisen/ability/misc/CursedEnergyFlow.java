581b63460540801dc95eba78646392095f3073cb
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-01-25 15:04:54 -0500

    nerfed CE flow defense again

--- src/main/java/radon/jujutsu_kaisen/ability/misc/CursedEnergyFlow.java
+++ src/main/java/radon/jujutsu_kaisen/ability/misc/CursedEnergyFlow.java
@@ -205,7 +205,7 @@ public class CursedEnergyFlow extends Ability implements Ability.IToggled {
                     ISorcererData attackerCap = attacker.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
 
                     if (HelperMethods.isMelee(source)) {
-                        float increase = attackerCap.getExperience() * 0.0005F;
+                        float increase = attackerCap.getExperience() * 0.0001F;
 
                         switch (attackerCap.getNature()) {
                             case ROUGH -> increase *= 1.5F;
