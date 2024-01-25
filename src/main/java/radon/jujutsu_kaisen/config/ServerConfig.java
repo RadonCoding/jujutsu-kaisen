e46ef8e474f3b36f7bcb91b4a3a37af1dea7b3c7
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-01-25 00:40:35 -0500

    Update ServerConfig.java

--- src/main/java/radon/jujutsu_kaisen/config/ServerConfig.java
+++ src/main/java/radon/jujutsu_kaisen/config/ServerConfig.java
@@ -59,7 +59,7 @@ public class ServerConfig {
         this.cursedEnergyAmount = builder.comment("Cursed energy amount (scales with experience)")
                 .defineInRange("cursedEnergyAmount", 500.0F, 0.0F, 100000.0F);
         this.cursedEnergyRegenerationAmount = builder.comment("Cursed energy regeneration amount (depends on food level)")
-                .defineInRange("cursedEnergyRegenerationAmount", 0.25F, 0.0F, 100000.0F);
+                .defineInRange("cursedEnergyRegenerationAmount", 0.5F, 0.0F, 100000.0F);
         this.maximumExperienceAmount = builder.comment("The maximum amount of experience one can obtain")
                 .defineInRange("maximumExperienceAmount", 10000.0F, 1.0F, 1000000.0F);
         this.cursedObjectEnergyForGrade = builder.comment("The amount of energy consuming cursed objects gives to curses (multiplied by the grade of the object)")
