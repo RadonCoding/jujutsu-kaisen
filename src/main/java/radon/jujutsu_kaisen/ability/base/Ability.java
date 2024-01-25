4626e61cf3db353b770cb994d00ca19fa46e1c07
codeblad <152137808+codeblad@users.noreply.github.com>
2024-01-25 00:28:11 -0500

    Update Ability.java
    removed six eyes half cooldown and point cost

--- src/main/java/radon/jujutsu_kaisen/ability/base/Ability.java
+++ src/main/java/radon/jujutsu_kaisen/ability/base/Ability.java
@@ -84,9 +84,9 @@ public abstract class Ability {
     public int getRealPointsCost(LivingEntity owner) {
         ISorcererData cap = owner.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
 
-        if (cap.hasTrait(Trait.SIX_EYES)) {
+        /*if (cap.hasTrait(Trait.SIX_EYES)) {
             return this.getPointsCost() / 2;
-        }
+        }*/
         return this.getPointsCost();
     }
 
@@ -157,7 +157,7 @@ public abstract class Ability {
     public int getRealCooldown(LivingEntity owner) {
         ISorcererData cap = owner.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
 
-        if ((this.isMelee() && cap.hasTrait(Trait.HEAVENLY_RESTRICTION)) || (this.getCost(owner) > 0.0F && cap.hasTrait(Trait.SIX_EYES))) {
+        if (this.isMelee() && cap.hasTrait(Trait.HEAVENLY_RESTRICTION)) {
             return this.getCooldown() / 2;
         }
         return this.getCooldown();
