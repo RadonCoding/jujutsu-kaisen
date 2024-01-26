cbc7f99ea391bc00c6f4c813bfd312af8f20d863
RadonCoding <henry.welch@reborn.com>
2024-01-26 18:51:40 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/ability/disaster_tides/EelGrapple.java
+++ src/main/java/radon/jujutsu_kaisen/ability/disaster_tides/EelGrapple.java
@@ -15,7 +15,9 @@ import radon.jujutsu_kaisen.util.HelperMethods;
 public class EelGrapple extends Ability {
     @Override
     public boolean shouldTrigger(PathfinderMob owner, @Nullable LivingEntity target) {
-        return HelperMethods.RANDOM.nextInt(3) == 0 && target != null && owner.hasLineOfSight(target) && owner.distanceTo(target) <= EelGrappleProjectile.RANGE;
+        if (target == null) return false;
+        if (!owner.hasLineOfSight(target) || owner.distanceTo(target) > EelGrappleProjectile.RANGE) return false;
+        return HelperMethods.RANDOM.nextInt(3) == 0;
     }
 
     @Override
