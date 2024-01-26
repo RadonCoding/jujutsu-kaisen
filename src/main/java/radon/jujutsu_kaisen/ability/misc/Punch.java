d66ada007ab2a143467db26f6e6933df416ff9c4
RadonCoding <henry.welch@reborn.com>
2024-01-26 02:52:36 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/ability/misc/Punch.java
+++ src/main/java/radon/jujutsu_kaisen/ability/misc/Punch.java
@@ -29,7 +29,7 @@ import radon.jujutsu_kaisen.util.RotationUtil;
 
 import java.util.List;
 
-public class Punch extends Ability implements Ability.ICharged {
+public class Punch extends Ability {
     private static final float DAMAGE = 5.0F;
     private static final double RANGE = 5.0D;
     private static final double LAUNCH_POWER = 2.5D;
@@ -58,12 +58,80 @@ public class Punch extends Ability implements Ability.ICharged {
 
     @Override
     public ActivationType getActivationType(LivingEntity owner) {
-        return ActivationType.CHANNELED;
+        return ActivationType.INSTANT;
+    }
+
+    private List<LivingEntity> getTargets(LivingEntity owner) {
+        Vec3 look = RotationUtil.getTargetAdjustedLookAngle(owner);
+        Vec3 offset = owner.getEyePosition().add(look.scale(RANGE / 2));
+        return owner.level().getEntitiesOfClass(LivingEntity.class, AABB.ofSize(offset, RANGE, RANGE, RANGE),
+                entity -> entity != owner && entity.hasLineOfSight(owner));
     }
 
     @Override
     public void run(LivingEntity owner) {
+        if (!(owner.level() instanceof ServerLevel level)) return;
 
+        Vec3 look = RotationUtil.getTargetAdjustedLookAngle(owner);
+
+        for (int i = 0; i < 4; i++) {
+            Vec3 pos = owner.getEyePosition().add(look.scale(2.5D));
+            level.sendParticles(owner.getItemInHand(InteractionHand.MAIN_HAND).getItem() instanceof SwordItem ? ParticleTypes.SWEEP_ATTACK : ParticleTypes.CLOUD,
+                    pos.x + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
+                    pos.y + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
+                    pos.z + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
+                    0, 0.0D, 0.0D, 0.0D, 1.0D);
+        }
+        for (int i = 0; i < 4; i++) {
+            Vec3 pos = owner.getEyePosition().add(look.scale(2.5D));
+            level.sendParticles(ParticleTypes.CRIT,
+                    pos.x + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
+                    pos.y + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
+                    pos.z + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
+                    0, 0.0D, 0.0D, 0.0D, 1.0D);
+        }
+
+        List<LivingEntity> targets = this.getTargets(owner);
+
+        for (LivingEntity entity : targets) {
+            Vec3 pos = entity.position().add(0.0D, entity.getBbHeight() / 2.0F, 0.0D);
+            level.sendParticles(ParticleTypes.EXPLOSION, pos.x, pos.y, pos.z, 0, 1.0D, 0.0D, 0.0D, 1.0D);
+            entity.level().playSound(null, pos.x, pos.y, pos.z, SoundEvents.GENERIC_EXPLODE, SoundSource.MASTER, 1.0F, 1.0F);
+
+            owner.swing(InteractionHand.MAIN_HAND, true);
+
+            if (owner instanceof Player player) {
+                player.attack(entity);
+            } else {
+                owner.doHurtTarget(entity);
+            }
+            entity.invulnerableTime = 0;
+
+            if (JJKAbilities.hasTrait(owner, Trait.HEAVENLY_RESTRICTION)) {
+                if (entity.hurt(owner instanceof Player player ? owner.damageSources().playerAttack(player) : owner.damageSources().mobAttack(owner), DAMAGE * this.getPower(owner))) {
+                    entity.setDeltaMovement(look.scale(LAUNCH_POWER * (1.0F + this.getPower(owner) * 0.1F) * 2.0F)
+                            .multiply(1.0D, 0.25D, 1.0D));
+                }
+            } else {
+                if (entity.hurt(JJKDamageSources.jujutsuAttack(owner, this), DAMAGE * this.getPower(owner))) {
+                    entity.setDeltaMovement(look.scale(LAUNCH_POWER * (1.0F + this.getPower(owner) * 0.1F))
+                            .multiply(1.0D, 0.25D, 1.0D));
+                }
+            }
+        }
+    }
+
+    @Override
+    public Status isTriggerable(LivingEntity owner) {
+        if (owner.isUsingItem()) {
+            return Status.FAILURE;
+        }
+        return super.isTriggerable(owner);
+    }
+
+    @Override
+    public boolean isValid(LivingEntity owner) {
+        return (!(owner instanceof ISorcerer sorcerer) || sorcerer.hasMeleeAttack() && sorcerer.hasArms()) && super.isValid(owner);
     }
 
     @Override
@@ -79,72 +147,5 @@ public class Punch extends Ability implements Ability.ICharged {
     @Override
     public MenuType getMenuType() {
         return MenuType.MELEE;
-    }
-
-    @Override
-    public boolean isValid(LivingEntity owner) {
-        return (!(owner instanceof ISorcerer sorcerer) || sorcerer.hasMeleeAttack() && sorcerer.hasArms()) && super.isValid(owner);
-    }
-
-    @Override
-    public boolean onRelease(LivingEntity owner) {
-        if (owner.isUsingItem()) return false;
-
-        Vec3 look = RotationUtil.getTargetAdjustedLookAngle(owner);
-
-        ISorcererData cap = owner.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
-
-        Vec3 offset = owner.getEyePosition().add(look.scale(RANGE / 2));
-
-        List<LivingEntity> targets = owner.level().getEntitiesOfClass(LivingEntity.class, AABB.ofSize(offset, RANGE, RANGE, RANGE), entity -> entity != owner && entity.hasLineOfSight(owner));
-
-        if (targets.isEmpty()) return false;
-
-        if (owner.level() instanceof ServerLevel level) {
-            for (int i = 0; i < 4; i++) {
-                Vec3 pos = owner.getEyePosition().add(look.scale(2.5D));
-                level.sendParticles(owner.getItemInHand(InteractionHand.MAIN_HAND).getItem() instanceof SwordItem ? ParticleTypes.SWEEP_ATTACK : ParticleTypes.CLOUD,
-                        pos.x + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
-                        pos.y + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
-                        pos.z + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
-                        0, 0.0D, 0.0D, 0.0D, 1.0D);
-            }
-            for (int i = 0; i < 4; i++) {
-                Vec3 pos = owner.getEyePosition().add(look.scale(2.5D));
-                level.sendParticles(ParticleTypes.CRIT,
-                        pos.x + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
-                        pos.y + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
-                        pos.z + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
-                        0, 0.0D, 0.0D, 0.0D, 1.0D);
-            }
-
-            for (LivingEntity entity : targets) {
-                Vec3 pos = entity.position().add(0.0D, entity.getBbHeight() / 2.0F, 0.0D);
-                level.sendParticles(ParticleTypes.EXPLOSION, pos.x, pos.y, pos.z, 0, 1.0D, 0.0D, 0.0D, 1.0D);
-                entity.level().playSound(null, pos.x, pos.y, pos.z, SoundEvents.GENERIC_EXPLODE, SoundSource.MASTER, 1.0F, 1.0F);
-
-                owner.swing(InteractionHand.MAIN_HAND, true);
-
-                if (owner instanceof Player player) {
-                    player.attack(entity);
-                } else {
-                    owner.doHurtTarget(entity);
-                }
-                entity.invulnerableTime = 0;
-
-                if (cap.hasTrait(Trait.HEAVENLY_RESTRICTION)) {
-                    if (entity.hurt(owner instanceof Player player ? owner.damageSources().playerAttack(player) : owner.damageSources().mobAttack(owner), DAMAGE * this.getPower(owner))) {
-                        entity.setDeltaMovement(look.scale(LAUNCH_POWER * (1.0F + this.getPower(owner) * 0.1F) * (cap.hasTrait(Trait.HEAVENLY_RESTRICTION) ? 2.0F : 1.0F))
-                                .multiply(1.0D, 0.25D, 1.0D));
-                    }
-                } else {
-                    if (entity.hurt(JJKDamageSources.jujutsuAttack(owner, this), DAMAGE * this.getPower(owner))) {
-                        entity.setDeltaMovement(look.scale(LAUNCH_POWER * (1.0F + this.getPower(owner) * 0.1F) * (cap.hasTrait(Trait.HEAVENLY_RESTRICTION) ? 2.0F : 1.0F))
-                                .multiply(1.0D, 0.25D, 1.0D));
-                    }
-                }
-            }
-        }
-        return true;
     }
 }
