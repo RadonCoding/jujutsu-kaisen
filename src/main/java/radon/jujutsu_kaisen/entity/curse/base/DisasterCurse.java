fb17f0c3f7508599b71debdb82b9aea3f99a584b
RadonCoding <henry.welch@reborn.com>
2024-01-29 17:17:40 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/entity/curse/base/DisasterCurse.java
+++ src/main/java/radon/jujutsu_kaisen/entity/curse/base/DisasterCurse.java
@@ -12,7 +12,7 @@ import radon.jujutsu_kaisen.capability.data.sorcerer.SorcererGrade;
 import radon.jujutsu_kaisen.util.SorcererUtil;
 
 public abstract class DisasterCurse extends CursedSpirit {
-    private static final int RARITY = 3;
+    private static final int RARITY = 5;
 
     protected DisasterCurse(EntityType<? extends TamableAnimal> pType, Level pLevel) {
         super(pType, pLevel);
