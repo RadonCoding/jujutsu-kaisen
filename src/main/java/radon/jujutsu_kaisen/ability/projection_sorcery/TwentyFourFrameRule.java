8ecd2efe9905419b798f1a695574b276876355a2
RadonCoding <henry.welch@reborn.com>
2024-01-29 14:08:05 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/ability/projection_sorcery/TwentyFourFrameRule.java
+++ src/main/java/radon/jujutsu_kaisen/ability/projection_sorcery/TwentyFourFrameRule.java
@@ -15,6 +15,7 @@ import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -110,15 +111,13 @@ public class TwentyFourFrameRule extends Ability implements Ability.IToggled, Ab
     @Mod.EventBusSubscriber(modid = JujutsuKaisen.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
     public static class TwentyFourFrameRuleForgeEvents {
         @SubscribeEvent
-        public static void onLivingDamage(LivingDamageEvent event) {
+        public static void onLivingAttack(LivingAttackEvent event) {
             DamageSource source = event.getSource();
             if (!(source.getEntity() instanceof LivingEntity attacker)) return;
 
             LivingEntity victim = event.getEntity();
 
             if (victim.level().isClientSide) return;
-
-            if (victim.getHealth() - event.getAmount() <= 0.0F) return;
 
             for (ProjectionFrameEntity frame : victim.level().getEntitiesOfClass(ProjectionFrameEntity.class, AABB.ofSize(victim.position(),
                     8.0D, 8.0D, 8.0D))) {
@@ -133,11 +132,15 @@ public class TwentyFourFrameRule extends Ability implements Ability.IToggled, Ab
 
                 LivingEntity owner = frame.getOwner();
 
-                if (owner != null) {
-                    victim.hurt(JJKDamageSources.indirectJujutsuAttack(frame, attacker, JJKAbilities.TWENTY_FOUR_FRAME_RULE.get()), DAMAGE * frame.getPower());
+                if (owner == null) continue;
+
+                if (victim.hurt(JJKDamageSources.indirectJujutsuAttack(frame, attacker, JJKAbilities.TWENTY_FOUR_FRAME_RULE.get()), DAMAGE * frame.getPower())) {
+                    if (victim.isDeadOrDying()) {
+                        event.setCanceled(true);
+                        return;
+                    }
                 }
-                event.setCanceled(true);
-                return;
+                break;
             }
         }
     }
