f8327cea3e3d7af1880369622d760309ac2f8b6d
RadonCoding <henry.welch@reborn.com>
2024-02-01 10:26:11 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/ability/disaster_flames/DisasterFlames.java
+++ src/main/java/radon/jujutsu_kaisen/ability/disaster_flames/DisasterFlames.java
@@ -14,6 +14,9 @@ import org.jetbrains.annotations.Nullable;
 import radon.jujutsu_kaisen.ability.JJKAbilities;
 import radon.jujutsu_kaisen.ability.base.Ability;
 import radon.jujutsu_kaisen.ability.MenuType;
+import radon.jujutsu_kaisen.capability.data.sorcerer.ISorcererData;
+import radon.jujutsu_kaisen.capability.data.sorcerer.SorcererDataHandler;
+import radon.jujutsu_kaisen.client.particle.FireParticle;
 import radon.jujutsu_kaisen.damage.JJKDamageSources;
 import radon.jujutsu_kaisen.util.HelperMethods;
 
@@ -33,25 +36,6 @@ public class DisasterFlames extends Ability implements Ability.IImbued {
         return ActivationType.INSTANT;
     }
 
-    private static void spawnParticles(Entity entity) {
-        double x = entity.getX();
-        double y = entity.getY() + (entity.getBbHeight() / 2.0F);
-        double z = entity.getZ();
-
-        for (int i = 0; i < 32; i++) {
-            double scale = HelperMethods.RANDOM.nextDouble() * 0.5D + 0.5D;
-            Vec3 speed = new Vec3(HelperMethods.RANDOM.nextGaussian(), HelperMethods.RANDOM.nextGaussian(), HelperMethods.RANDOM.nextGaussian())
-                    .normalize().scale(scale);
-
-            double offsetX = x + speed.x;
-            double offsetY = y + speed.y;
-            double offsetZ = z + speed.z;
-
-            ((ServerLevel) entity.level()).sendParticles((ParticleOptions) ParticleTypes.FLAME, offsetX, offsetY, offsetZ, 0,
-                    speed.x, speed.y, speed.z, 1.0D);
-        }
-    }
-
     private List<LivingEntity> getTargets(LivingEntity owner) {
         return owner.level().getEntitiesOfClass(LivingEntity.class, owner.getBoundingBox().inflate(AOE_RANGE), entity -> entity != owner);
     }
@@ -68,11 +52,31 @@ public class DisasterFlames extends Ability implements Ability.IImbued {
 
     @Override
     public void run(LivingEntity owner, Entity target) {
-        owner.level().playSound(null, target.getX(), target.getY(), target.getZ(), SoundEvents.FIRECHARGE_USE, SoundSource.MASTER, 1.0F, 1.0F);
+        ISorcererData cap = owner.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
 
         if (target.hurt(JJKDamageSources.indirectJujutsuAttack(owner, owner, JJKAbilities.DISASTER_FLAMES.get()), DAMAGE * Ability.getPower(JJKAbilities.DISASTER_FLAMES.get(), owner) * (float) (1.0F - (target.distanceTo(owner) / AOE_RANGE)))) {
             target.setSecondsOnFire(5);
-            spawnParticles(target);
+
+            for (int i = 0; i < 8; i++) {
+                cap.delayTickEvent(() -> {
+                    owner.level().playSound(null, target.getX(), target.getY(), target.getZ(), SoundEvents.FIRECHARGE_USE, SoundSource.MASTER, 1.0F, 1.0F);
+
+                    double x = target.getX();
+                    double y = target.getY();
+                    double z = target.getZ();
+
+                    for (int j = 0; j < 32; j++) {
+                        Vec3 speed = new Vec3((HelperMethods.RANDOM.nextDouble() - 0.5D) * 0.25D, HelperMethods.RANDOM.nextDouble() * 0.5D, (HelperMethods.RANDOM.nextDouble() - 0.5D) * 0.25D);
+
+                        double offsetX = x + speed.x;
+                        double offsetY = y + speed.y;
+                        double offsetZ = z + speed.z;
+
+                        ((ServerLevel) target.level()).sendParticles(new FireParticle.FireParticleOptions(target.getBbWidth(), true, 20), offsetX, offsetY, offsetZ, 0,
+                                speed.x, speed.y, speed.z, 1.0D);
+                    }
+                }, i * 2);
+            }
         }
     }
 
