e07983e3532c0c40495a613256a0e756f4576b55
RadonCoding <henry.welch@reborn.com>
2024-02-02 07:57:28 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/entity/projectile/HollowPurpleProjectile.java
+++ src/main/java/radon/jujutsu_kaisen/entity/projectile/HollowPurpleProjectile.java
@@ -28,7 +28,7 @@ public class HollowPurpleProjectile extends JujutsuProjectile {
     private static final int DURATION = 5 * 20;
     private static final float DAMAGE = 30.0F;
     private static final float MAX_RADIUS = 15.0F;
-    private static final float RADIUS = 1.0F;
+    private static final float RADIUS = 2.0F;
     private static final int ANIMATION = 20;
 
     public HollowPurpleProjectile(EntityType<? extends Projectile> pType, Level pLevel) {
