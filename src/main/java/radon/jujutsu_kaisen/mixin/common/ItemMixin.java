7ca0363c9afd173ffa7cf4b7939d3196115b9e65
RadonCoding <henry.welch@reborn.com>
2024-02-13 15:04:58 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/mixin/common/ItemMixin.java
+++ src/main/java/radon/jujutsu_kaisen/mixin/common/ItemMixin.java
@@ -12,6 +12,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import radon.jujutsu_kaisen.ImbuementHandler;
 import radon.jujutsu_kaisen.JujutsuKaisen;
+import radon.jujutsu_kaisen.ability.base.Ability;
 import radon.jujutsu_kaisen.cursed_technique.base.ICursedTechnique;
 import radon.jujutsu_kaisen.config.ConfigHolder;
 
@@ -22,7 +23,7 @@ import java.util.Map;
 public class ItemMixin {
     @Inject(method = "appendHoverText", at = @At("HEAD"))
     public void appendHoverText(ItemStack pStack, Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced, CallbackInfo ci) {
-        for (Map.Entry<ICursedTechnique, Integer> entry : ImbuementHandler.getImbuementAmounts(pStack).entrySet()) {
+        for (Map.Entry<Ability, Integer> entry : ImbuementHandler.getImbuementAmounts(pStack).entrySet()) {
             pTooltipComponents.add(Component.translatable(String.format("item.%s.imbuement", JujutsuKaisen.MOD_ID), entry.getKey().getName().copy().withStyle(ChatFormatting.DARK_PURPLE),
                     (float) entry.getValue() / ConfigHolder.SERVER.requiredImbuementAmount.get() * 100));
         }
