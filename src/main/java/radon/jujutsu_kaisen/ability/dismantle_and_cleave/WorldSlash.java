7a78b77b7f84fc17bb6a2d51b9b288264a03daab
RadonCoding <henry.welch@reborn.com>
2024-02-20 15:15:53 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/ability/dismantle_and_cleave/WorldSlash.java
+++ src/main/java/radon/jujutsu_kaisen/ability/dismantle_and_cleave/WorldSlash.java
@@ -60,7 +60,7 @@ public class WorldSlash extends Ability {
     @Override
     public Status isTriggerable(LivingEntity owner) {
         if (!(owner instanceof MahoragaEntity)) {
-            if (ChantHandler.getOutput(owner, this) < 2.0F) return Status.FAILURE;
+            if (ChantHandler.getOutput(owner, this) < 1.2F) return Status.FAILURE;
         }
         return super.isTriggerable(owner);
     }
