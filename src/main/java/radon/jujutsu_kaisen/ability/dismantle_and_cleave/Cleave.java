4146777c63586a0a735b705b0dfa93849d73f82f
RadonCoding <henry.welch@reborn.com>
2024-02-27 08:46:23 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/ability/dismantle_and_cleave/Cleave.java
+++ src/main/java/radon/jujutsu_kaisen/ability/dismantle_and_cleave/Cleave.java
@@ -94,7 +94,7 @@ public class Cleave extends Ability implements Ability.IDomainAttack, Ability.IA
 
     @Override
     public int getCooldown() {
-        return 5 * 20;
+        return 10 * 20;
     }
 
     @Override
