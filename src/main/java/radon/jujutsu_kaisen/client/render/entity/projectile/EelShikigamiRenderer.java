154bfc79175e484c113c0082fc408cc0e342f82a
RadonCoding <henry.welch@reborn.com>
2024-02-28 16:28:12 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/client/render/entity/projectile/EelShikigamiRenderer.java
+++ src/main/java/radon/jujutsu_kaisen/client/render/entity/projectile/EelShikigamiRenderer.java
@@ -8,7 +8,6 @@ import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import radon.jujutsu_kaisen.JujutsuKaisen;
-import radon.jujutsu_kaisen.client.layer.EelShikigamiEyesLayer;
 import radon.jujutsu_kaisen.entity.projectile.EelShikigamiProjectile;
 import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.model.DefaultedEntityGeoModel;
@@ -17,12 +16,12 @@ import software.bernie.geckolib.renderer.GeoEntityRenderer;
 public class EelShikigamiRenderer extends GeoEntityRenderer<EelShikigamiProjectile> {
     public EelShikigamiRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new DefaultedEntityGeoModel<>(new ResourceLocation(JujutsuKaisen.MOD_ID, "eel_shikigami")));
-
-        this.addRenderLayer(new EelShikigamiEyesLayer(this));
     }
 
     @Override
     public void preRender(PoseStack poseStack, EelShikigamiProjectile animatable, BakedGeoModel model, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        poseStack.translate(0.0F, animatable.getBbHeight() / 2.0F, 0.0F);
+
         float yaw = Mth.lerp(partialTick, animatable.yRotO, animatable.getYRot());
         float pitch = Mth.lerp(partialTick, animatable.xRotO, animatable.getXRot());
 
