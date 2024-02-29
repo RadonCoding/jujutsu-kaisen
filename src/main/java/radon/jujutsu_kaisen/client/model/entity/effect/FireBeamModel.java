b3b0b9bcfc6e28e00c67fb7eb9b6b2b80bfe61e7
RadonCoding <henry.welch@reborn.com>
2024-02-29 17:45:04 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/client/model/entity/effect/FireBeamModel.java
+++ src/main/java/radon/jujutsu_kaisen/client/model/entity/effect/FireBeamModel.java
@@ -46,7 +46,7 @@ public class FireBeamModel extends EntityModel<FireBeamEntity> {
                 PartPose.offset(0.0F, 2.0F, 0.0F));
         part.addOrReplaceChild("body4", CubeListBuilder.create()
                 .texOffs(0, 0)
-                .addBox(-4.0F, -4.0F, -8.0F, 8, 8, 8, CubeDeformation.NONE),
+                .addBox(-4.0F, -4.0F, -4.0F, 8, 8, 8, CubeDeformation.NONE),
                 PartPose.offset(0.0F, 2.0F, 0.0F));
 
         return LayerDefinition.create(mesh, 16, 16);
