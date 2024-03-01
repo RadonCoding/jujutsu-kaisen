5ded09ec10492d93c2cdd755fcb8b834122bbb0d
RadonCoding <henry.welch@reborn.com>
2024-03-01 08:54:01 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/entity/projectile/BodyRepelProjectile.java
+++ src/main/java/radon/jujutsu_kaisen/entity/projectile/BodyRepelProjectile.java
@@ -40,7 +40,7 @@ import software.bernie.geckolib.util.GeckoLibUtil;
 public class BodyRepelProjectile extends Projectile implements GeoEntity {
     private static final EntityDataAccessor<Integer> DATA_TIME = SynchedEntityData.defineId(BodyRepelProjectile.class, EntityDataSerializers.INT);
 
-    private static final double SPEED = 3.0D;
+    private static final double SPEED = 0.0D;
     private static final float DAMAGE = 10.0F;
 
     private static final int MAX_SEGMENTS = 24;
@@ -137,7 +137,7 @@ public class BodyRepelProjectile extends Projectile implements GeoEntity {
             Entity leader = i == 0 ? this : this.segments[i - 1];
             Vec3 follow = leader.position();
 
-            float angle = (((leader.getYRot() + 180.0F) * Mth.PI) / 180.0F);
+            float angle = ((leader.getYRot() * Mth.PI) / 180.0F);
 
             double f = (leader.getBbWidth() / 2) + (this.segments[i].getBbWidth() / 2);
 
