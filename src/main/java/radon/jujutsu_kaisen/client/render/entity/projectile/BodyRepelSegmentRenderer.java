aa9e622c3feb80a524e1b97732d7338d7908e075
RadonCoding <henry.welch@reborn.com>
2024-03-01 11:51:24 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/client/render/entity/projectile/BodyRepelSegmentRenderer.java
+++ src/main/java/radon/jujutsu_kaisen/client/render/entity/projectile/BodyRepelSegmentRenderer.java
@@ -17,19 +17,8 @@ import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.model.DefaultedEntityGeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
-public class BodyRepelSegmentRenderer extends GeoEntityRenderer<BodyRepelSegmentEntity> {
+public class BodyRepelSegmentRenderer extends SegmentRenderer<BodyRepelSegmentEntity> {
     public BodyRepelSegmentRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new DefaultedEntityGeoModel<>(new ResourceLocation(JujutsuKaisen.MOD_ID, "body_repel_segment")));
-    }
-
-    @Override
-    public void preRender(PoseStack poseStack, BodyRepelSegmentEntity animatable, BakedGeoModel model, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-        float yaw = Mth.lerp(partialTick, animatable.yRotO, animatable.getYRot());
-        float pitch = Mth.lerp(partialTick, animatable.xRotO, animatable.getXRot());
-
-        poseStack.mulPose(Axis.YP.rotationDegrees(yaw));
-        poseStack.mulPose(Axis.XN.rotationDegrees(pitch));
-
-        super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
     }
 }
