2b15e176e1e2e7f6dceffceceb16559aa84f6b7c
RadonCoding <henry.welch@reborn.com>
2024-03-01 12:01:56 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/client/JJKClientEventHandler.java
+++ src/main/java/radon/jujutsu_kaisen/client/JJKClientEventHandler.java
@@ -346,7 +346,6 @@ public class JJKClientEventHandler {
             event.registerEntityRenderer(JJKEntities.CURSED_ENERGY_BLAST.get(), CursedEnergyBlastRenderer::new);
             event.registerEntityRenderer(JJKEntities.EEL_GRAPPLE.get(), EelGrappleRenderer::new);
             event.registerEntityRenderer(JJKEntities.TRANSFIGURED_SOUL.get(), TransfiguredSoulRenderer::new);
-            event.registerEntityRenderer(JJKEntities.BODY_REPEL.get(), BodyRepelHeadRenderer::new);
 
             event.registerBlockEntityRenderer(JJKBlockEntities.DISPLAY_CASE.get(), DisplayCaseBlockRenderer::new);
             event.registerBlockEntityRenderer(JJKBlockEntities.UNLIMITED_VOID.get(), UnlimitedVoidBlockRenderer::new);
