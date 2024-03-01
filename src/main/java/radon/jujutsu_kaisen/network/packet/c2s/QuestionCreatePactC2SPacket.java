4e7da4a42a93f9a0e53afa3622042930be4cea8c
RadonCoding <henry.welch@reborn.com>
2024-03-01 06:45:25 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/network/packet/c2s/QuestionCreatePactC2SPacket.java
+++ src/main/java/radon/jujutsu_kaisen/network/packet/c2s/QuestionCreatePactC2SPacket.java
@@ -55,15 +55,17 @@ public class QuestionCreatePactC2SPacket implements CustomPacketPayload {
             if (player != null) {
                 Component accept = Component.translatable(String.format("chat.%s.pact_question_accept", JujutsuKaisen.MOD_ID))
                         .withStyle(Style.EMPTY.withColor(ChatFormatting.GREEN)
-                                .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, String.format("/pactcreationaccept %s %s", sender.getName().getString(), this.pact.getName())))
+                                .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, String.format("/pactcreationaccept %s %s",
+                                        sender.getName().getString(), JJKPacts.getKey(this.pact).toString())))
                                 .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.translatable(String.format("chat.%s.pact_question_accept", JujutsuKaisen.MOD_ID)))));
                 Component decline = Component.translatable(String.format("chat.%s.pact_question_decline", JujutsuKaisen.MOD_ID))
                         .withStyle(Style.EMPTY.withColor(ChatFormatting.RED)
-                                .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, String.format("/pactcreationdecline %s %s", sender.getName().getString(), this.pact.getName())))
+                                .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, String.format("/pactcreationdecline %s %s",
+                                        sender.getName().getString(), JJKPacts.getKey(this.pact).toString())))
                                 .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.translatable(String.format("chat.%s.pact_question_decline", JujutsuKaisen.MOD_ID)))));
 
                 Component message = Component.translatable(String.format("chat.%s.pact_question_create", JujutsuKaisen.MOD_ID), accept, decline,
-                        this.pact.getName().getString().toLowerCase(), sender.getName());
+                        JJKPacts.getKey(this.pact).toString(), sender.getName());
 
                 player.sendSystemMessage(message);
             }
