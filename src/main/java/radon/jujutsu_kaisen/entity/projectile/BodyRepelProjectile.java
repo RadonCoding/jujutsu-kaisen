37b97bb2a00f804ce9ad0407add0820ff766bf95
RadonCoding <henry.welch@reborn.com>
2024-03-01 09:28:13 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/entity/projectile/BodyRepelProjectile.java
+++ src/main/java/radon/jujutsu_kaisen/entity/projectile/BodyRepelProjectile.java
@@ -41,7 +41,7 @@ import software.bernie.geckolib.util.GeckoLibUtil;
 public class BodyRepelProjectile extends Projectile implements GeoEntity {
     private static final EntityDataAccessor<Integer> DATA_TIME = SynchedEntityData.defineId(BodyRepelProjectile.class, EntityDataSerializers.INT);
 
-    private static final double SPEED = 3.0D;
+    private static final double SPEED = 1.0D;
     private static final float DAMAGE = 10.0F;
 
     private static final int MAX_SEGMENTS = 24;
@@ -149,7 +149,7 @@ public class BodyRepelProjectile extends Projectile implements GeoEntity {
 
             float angle = (((leader.getYRot() + 180.0F) * Mth.PI) / 180.0F);
 
-            double f = (leader.getBbWidth() / 2) + (this.segments[i].getBbWidth() / 2);
+            double f = i == 0 ? leader.getBbWidth() / 2 : (leader.getBbWidth() / 2) + (this.segments[i].getBbWidth() / 2);
 
             double force = 0.05D + (1.0D / (i + 1)) * 0.5D;
 
@@ -169,7 +169,7 @@ public class BodyRepelProjectile extends Projectile implements GeoEntity {
             this.segments[i].setPos(destX, destY, destZ);
 
             double distance = Mth.sqrt((float) (diff.x * diff.x + diff.z * diff.z));
-            this.segments[i].setRot((float) (Math.atan2(diff.z, diff.x) * 180.0D / Math.PI) - 180.0F, -(float) (Math.atan2(diff.y, distance) * 180.0D / Math.PI));
+            this.segments[i].setRot((float) (Math.atan2(diff.z, diff.x) * 180.0D / Math.PI) + 90.0F, -(float) (Math.atan2(diff.y, distance) * 180.0D / Math.PI));
         }
     }
 
