427464c284eac1ce823ee0d006eec38a1e6fe0df
RadonCoding <henry.welch@reborn.com>
2024-03-01 11:01:49 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/client/render/entity/projectile/BodyRepelSegmentRenderer.java
+++ src/main/java/radon/jujutsu_kaisen/client/render/entity/projectile/BodyRepelSegmentRenderer.java
@@ -1,15 +1,43 @@
 package radon.jujutsu_kaisen.client.render.entity.projectile;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.LivingEntity;
 import radon.jujutsu_kaisen.JujutsuKaisen;
 import radon.jujutsu_kaisen.client.render.entity.base.SegmentRenderer;
 import radon.jujutsu_kaisen.entity.curse.WormCurseSegmentEntity;
 import radon.jujutsu_kaisen.entity.projectile.BodyRepelSegmentEntity;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.model.DefaultedEntityGeoModel;
+import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
-public class BodyRepelSegmentRenderer extends SegmentRenderer<BodyRepelSegmentEntity> {
+public class BodyRepelSegmentRenderer extends GeoEntityRenderer<BodyRepelSegmentEntity> {
     public BodyRepelSegmentRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new DefaultedEntityGeoModel<>(new ResourceLocation(JujutsuKaisen.MOD_ID, "body_repel_segment")));
+    }
+
+    @Override
+    public void preRender(PoseStack poseStack, BodyRepelSegmentEntity animatable, BakedGeoModel model, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        float diff = animatable.getYRot() - animatable.yRotO;
+
+        if (diff > 180.0F) {
+            diff -= 360.0F;
+        } else if (diff < -180.0F) {
+            diff += 360.0F;
+        }
+
+        float yaw = animatable.yRotO + diff * partialTick;
+        float pitch = Mth.lerp(partialTick, animatable.xRotO, animatable.getXRot());
+
+        poseStack.mulPose(Axis.YP.rotationDegrees(yaw));
+        poseStack.mulPose(Axis.XN.rotationDegrees(pitch));
+
+        super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
     }
 }
