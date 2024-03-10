d876b65ca230988c4bb3818bfacc42d48d7f9ac6
RadonCoding <henry.welch@reborn.com>
2024-03-10 15:41:23 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/entity/projectile/FireArrowProjectile.java
+++ src/main/java/radon/jujutsu_kaisen/entity/projectile/FireArrowProjectile.java
@@ -27,7 +27,7 @@ import radon.jujutsu_kaisen.util.ParticleUtil;
 import radon.jujutsu_kaisen.util.RotationUtil;
 
 public class FireArrowProjectile extends JujutsuProjectile {
-    private static final float DAMAGE = 25.0F;
+    private static final float DAMAGE = 20.0F;
     private static final float SPEED = 5.0F;
     private static final float EXPLOSIVE_POWER = 2.5F;
     private static final float MAX_EXPLOSION = 15.0F;
