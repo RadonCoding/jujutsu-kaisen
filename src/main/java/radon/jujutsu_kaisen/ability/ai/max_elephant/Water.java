44ed8b14c90705d969294811d644cbabd43d1d1d
RadonCoding <henry.welch@reborn.com>
2024-03-10 16:01:35 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/ability/ai/max_elephant/Water.java
+++ src/main/java/radon/jujutsu_kaisen/ability/ai/max_elephant/Water.java
@@ -35,7 +35,7 @@ import java.util.Optional;
 public class Water extends Ability implements Ability.IChannelened, Ability.IDurationable {
     public static final double RANGE = 32;
     private static final float SCALE = 2.0F;
-    private static final float DAMAGE = 1.0F;
+    private static final float DAMAGE = 2.0F;
 
     @Override
     public boolean isScalable(LivingEntity owner) {
