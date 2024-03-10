03ca483102bee369a448813307bc5224fc812f0b
RadonCoding <henry.welch@reborn.com>
2024-03-10 15:34:37 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/ability/dismantle_and_cleave/Cleave.java
+++ src/main/java/radon/jujutsu_kaisen/ability/dismantle_and_cleave/Cleave.java
@@ -148,7 +148,8 @@ public class Cleave extends Ability implements Ability.IDomainAttack, Ability.IA
 
             if (!success || !(target instanceof Mob) && !(target instanceof Player)) return;
 
-            owner.level().playSound(null, target.getX(), target.getY(), target.getZ(), JJKSounds.CLEAVE.get(), SoundSource.MASTER, 1.0F, 1.0F);
+            owner.level().playSound(null, target.getX(), target.getY(), target.getZ(),
+                    JJKSounds.CLEAVE.get(), SoundSource.MASTER, domain == null ? 1.0F : 0.05F, 1.0F);
         }, 20);
     }
 
