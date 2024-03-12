1909a7d2019229804b20d5e02ce2f945b80f5d46
RadonCoding <henry.welch@reborn.com>
2024-03-12 16:27:15 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/ability/disaster_flames/EmberInsectFlight.java
+++ src/main/java/radon/jujutsu_kaisen/ability/disaster_flames/EmberInsectFlight.java
@@ -5,8 +5,12 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.PathfinderMob;
 import org.jetbrains.annotations.Nullable;
 import radon.jujutsu_kaisen.ability.base.Summon;
+import radon.jujutsu_kaisen.data.ability.IAbilityData;
+import radon.jujutsu_kaisen.data.capability.IJujutsuCapability;
+import radon.jujutsu_kaisen.data.capability.JujutsuCapabilityHandler;
 import radon.jujutsu_kaisen.entity.EmberInsectFlightEntity;
 import radon.jujutsu_kaisen.entity.JJKEntities;
+import radon.jujutsu_kaisen.util.HelperMethods;
 
 import java.util.List;
 
@@ -17,6 +21,15 @@ public class EmberInsectFlight extends Summon<EmberInsectFlightEntity> {
 
     @Override
     public boolean shouldTrigger(PathfinderMob owner, @Nullable LivingEntity target) {
+        IJujutsuCapability cap = owner.getCapability(JujutsuCapabilityHandler.INSTANCE);
+
+        if (cap == null) return false;
+
+        IAbilityData data = cap.getAbilityData();
+
+        if (data.hasToggled(this)) {
+            return owner.getFeetBlockState().getCollisionShape(owner.level(), owner.blockPosition()).isEmpty() && HelperMethods.RANDOM.nextInt(5) != 0;
+        }
         return owner.fallDistance > 2.0F && !owner.isInFluidType();
     }
 
