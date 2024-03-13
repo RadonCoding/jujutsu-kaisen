1bdfcec108b8c0f5602042842b05fcf746826fc7
RadonCoding <henry.welch@reborn.com>
2024-03-13 19:57:36 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/event/WeaponEventHandler.java
+++ src/main/java/radon/jujutsu_kaisen/event/WeaponEventHandler.java
@@ -81,7 +81,7 @@ public class WeaponEventHandler {
                 if (attackerCap != null) {
                     ISorcererData attackerData = attackerCap.getSorcererData();
 
-                    if (attackerData.hasTrait(Trait.HEAVENLY_RESTRICTION)) {
+                    if (attackerData.hasTrait(Trait.HEAVENLY_RESTRICTION) || attackerData.getFingers() > 0) {
                         float amount = JJKItems.SPLIT_SOUL_KATANA.get().getDamage();
 
                         IJujutsuCapability victimCap = victim.getCapability(JujutsuCapabilityHandler.INSTANCE);
