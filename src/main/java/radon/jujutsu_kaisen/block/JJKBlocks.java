28866a71f48d039d43abe3743bd1471f949117b5
RadonCoding <henry.welch@reborn.com>
2024-03-15 00:43:57 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/block/JJKBlocks.java
+++ src/main/java/radon/jujutsu_kaisen/block/JJKBlocks.java
@@ -191,7 +191,6 @@ public class JJKBlocks {
     public static DeferredHolder<Block, ChimeraShadowGardenBlock> CHIMERA_SHADOW_GARDEN = BLOCKS.register("chimera_shadow_garden", () ->
             new ChimeraShadowGardenBlock(JJKFluids.CHIMERA_SHADOW_GARDEN_SOURCE, BlockBehaviour.Properties.ofFullCopy(Blocks.WATER)));
 
-
     public static DeferredHolder<Block, Block> VEIL = BLOCKS.register("veil", () ->
             new VeilBlock(BlockBehaviour.Properties.of()
                     .mapColor(state -> state.getValue(VeilBlock.COLOR).getMapColor())
