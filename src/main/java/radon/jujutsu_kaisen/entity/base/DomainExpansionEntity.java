d343f3d47c87fe794680e7b235ca0204e8202408
RadonCoding <henry.welch@reborn.com>
2024-03-18 21:05:09 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/entity/base/DomainExpansionEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/base/DomainExpansionEntity.java
@@ -248,7 +248,7 @@ public abstract class DomainExpansionEntity extends Entity {
                     ISkillData data = ownerCap.getSkillData();
 
                     simple.hurt(JJKDamageSources.indirectJujutsuAttack(this, owner, this.ability),
-                            data.getSkill(Skill.BARRIER) * 0.01F);
+                            (data.getSkill(Skill.BARRIER) * 0.01F) / 20);
                 }
             }
         }
