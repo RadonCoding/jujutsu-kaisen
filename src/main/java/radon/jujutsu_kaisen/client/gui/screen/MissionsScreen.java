68d3d0c388ee99d0f6033c3908126a4bbb2db54e
RadonCoding <henry.welch@reborn.com>
2024-03-20 16:05:12 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/client/gui/screen/MissionsScreen.java
+++ src/main/java/radon/jujutsu_kaisen/client/gui/screen/MissionsScreen.java
@@ -17,27 +17,28 @@ public class MissionsScreen extends Screen {
     private static final ResourceLocation WINDOW = new ResourceLocation(JujutsuKaisen.MOD_ID, "textures/gui/missions/window.png");
     private static final ResourceLocation BACKGROUND = new ResourceLocation("textures/gui/advancements/backgrounds/stone.png");
     private static final ResourceLocation MISSION_CARD = new ResourceLocation(JujutsuKaisen.MOD_ID, "textures/gui/missions/mission_card.png");
-    private static final ResourceLocation MISSION_GRADE_BUTTON = new ResourceLocation(JujutsuKaisen.MOD_ID, "textures/gui/missions/mission_grade_button.png");
+    private static final ResourceLocation MISSION_GRADE = new ResourceLocation(JujutsuKaisen.MOD_ID, "textures/gui/missions/mission_grade.png");
 
-    public static final int WINDOW_WIDTH = 78;
-    public static final int WINDOW_HEIGHT = 200;
+    private static final int WINDOW_WIDTH = 78;
+    private static final int WINDOW_HEIGHT = 200;
     private static final int WINDOW_INSIDE_X = 9;
     private static final int WINDOW_INSIDE_Y = 18;
     private static final int WINDOW_OFFSET_X = 20;
-    private static final int WINDOW_OFFSET_Y = 20;
     private static final int WINDOW_INSIDE_WIDTH = 60;
     private static final int WINDOW_INSIDE_HEIGHT = 173;
     private static final int BACKGROUND_TILE_WIDTH = 16;
     private static final int BACKGROUND_TILE_HEIGHT = 16;
 
-    public static final int MISSION_CARD_WIDTH = 104;
-    public static final int MISSION_CARD_HEIGHT = 128;
-
+    private static final int MISSION_CARD_WIDTH = 104;
+    private static final int MISSION_CARD_HEIGHT = 128;
+    private static final int MISSION_CARD_INSIDE_X = 9;
+    private static final int MISSION_CARD_INSIDE_Y = 18;
     private static final int MISSION_CARD_OFFSET_X = 20;
-    public static final int MISSION_CARD_PADDING = 10;
+    private static final int MISSION_CARD_PADDING = 10;
+    private static final int MISSION_CARD_INSIDE_WIDTH = 86;
+    private static final int MISSION_CARD_INSIDE_HEIGHT = 101;
 
-    private static final int MISSION_GRADE_BUTTON_WIDTH = 52;
-    private static final int MISSION_GRADE_BUTTON_HEIGHT = 52;
+    private static final int MISSION_GRADE_SIZE = 52;
 
     private final Map<MissionGrade, List<Mission>> missions;
 
@@ -102,7 +103,7 @@ public class MissionsScreen extends Screen {
 
         this.missionButtonsSlider = new VerticalSlider(windowOffsetX + WINDOW_INSIDE_X + WINDOW_INSIDE_WIDTH - 8, windowOffsetY + WINDOW_INSIDE_Y,
                 8, WINDOW_INSIDE_HEIGHT, Component.empty(), Component.empty(), 0.0D,
-                (MissionGrade.values().length * MISSION_GRADE_BUTTON_HEIGHT) - WINDOW_INSIDE_HEIGHT,
+                (MissionGrade.values().length * MISSION_GRADE_SIZE) - WINDOW_INSIDE_HEIGHT,
                 0, 0.1D, 0, false);
         this.addRenderableWidget(this.missionButtonsSlider);
 
@@ -129,8 +130,9 @@ public class MissionsScreen extends Screen {
         this.renderBackground(pGuiGraphics);
         this.renderWindowInside(pGuiGraphics, windowOffsetX, windowOffsetY);
         this.renderWindow(pGuiGraphics, windowOffsetX, windowOffsetY);
-        this.renderMissionButtons(pGuiGraphics, windowOffsetX, windowOffsetY);
-        this.renderMissionCards(pGuiGraphics, missionCardOffsetX, missionCardOffsetY);
+        this.renderMissionGrades(pGuiGraphics, windowOffsetX, windowOffsetY);
+        this.renderMissionCardsInside(pGuiGraphics, missionCardOffsetX, missionCardOffsetY);
+        this.renderMissionCardsWindow(pGuiGraphics, missionCardOffsetX, missionCardOffsetY);
         super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
     }
 
@@ -161,7 +163,7 @@ public class MissionsScreen extends Screen {
         graphics.blit(WINDOW, offsetX, offsetY, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
     }
 
-    private void renderMissionButtons(GuiGraphics graphics, int offsetX, int offsetY) {
+    private void renderMissionGrades(GuiGraphics graphics, int offsetX, int offsetY) {
         int x = offsetX + WINDOW_INSIDE_X;
         int y = offsetY + WINDOW_INSIDE_Y;
 
@@ -170,14 +172,39 @@ public class MissionsScreen extends Screen {
         graphics.pose().translate(0.0D, -this.missionButtonsSlider.getValue(), 0.0D);
 
         for (int i = 0; i < MissionGrade.values().length; i++) {
-            graphics.blit(MISSION_GRADE_BUTTON, x, y + i * MISSION_GRADE_BUTTON_HEIGHT, 0, 0,
-                    MISSION_GRADE_BUTTON_WIDTH, MISSION_GRADE_BUTTON_HEIGHT, MISSION_GRADE_BUTTON_WIDTH, MISSION_GRADE_BUTTON_HEIGHT);
+            graphics.blit(MISSION_GRADE, x, y + i * MISSION_GRADE_SIZE, this.grade.ordinal() == i ? MISSION_GRADE_SIZE : 0, 0,
+                    MISSION_GRADE_SIZE, MISSION_GRADE_SIZE);
         }
         graphics.pose().popPose();
         graphics.disableScissor();
     }
 
-    private void renderMissionCards(GuiGraphics graphics, int offsetX, int offsetY) {
+    private void renderMissionCardsInside(GuiGraphics graphics, int offsetX, int offsetY) {
+        graphics.enableScissor(offsetX, 0, this.width - MISSION_CARD_OFFSET_X, this.height);
+
+        List<MissionsScreen.Mission> missions = this.missions.get(this.grade);
+
+        for (int i = 0; i < missions.size(); i++) {
+            int x = offsetX - this.missionCardsSlider.getValueInt() + (i * (MISSION_CARD_WIDTH + MISSION_CARD_PADDING)) + MISSION_CARD_INSIDE_X;
+            int y = offsetY + MISSION_CARD_INSIDE_Y;
+
+            graphics.enableScissor(x, y, x + MISSION_CARD_INSIDE_WIDTH, y + MISSION_CARD_INSIDE_HEIGHT);
+            graphics.pose().pushPose();
+            graphics.pose().translate((float) x, (float) y, 0.0F);
+
+            for (int i1 = -1; i1 <= MISSION_CARD_INSIDE_WIDTH / BACKGROUND_TILE_WIDTH; ++i1) {
+                for (int j1 = -1; j1 <= MISSION_CARD_INSIDE_HEIGHT / BACKGROUND_TILE_HEIGHT; ++j1) {
+                    graphics.blit(BACKGROUND, BACKGROUND_TILE_WIDTH * i1, BACKGROUND_TILE_HEIGHT * j1, 0.0F, 0.0F,
+                            BACKGROUND_TILE_WIDTH, BACKGROUND_TILE_HEIGHT, BACKGROUND_TILE_WIDTH, BACKGROUND_TILE_HEIGHT);
+                }
+            }
+            graphics.pose().popPose();
+            graphics.disableScissor();
+        }
+        graphics.disableScissor();
+    }
+
+    private void renderMissionCardsWindow(GuiGraphics graphics, int offsetX, int offsetY) {
         RenderSystem.enableBlend();
 
         graphics.enableScissor(offsetX, 0, this.width - MISSION_CARD_OFFSET_X, this.height);
