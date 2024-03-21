274501320297449c8905c9c70639b67ac4903c15
RadonCoding <henry.welch@reborn.com>
2024-03-21 14:48:25 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/ability/misc/QuickDraw.java
+++ src/main/java/radon/jujutsu_kaisen/ability/misc/QuickDraw.java
@@ -14,15 +14,18 @@ import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.*;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.common.NeoForgeMod;
+import net.neoforged.neoforge.event.EventHooks;
 import net.neoforged.neoforge.event.entity.living.LivingAttackEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
+import net.neoforged.neoforge.event.entity.living.LivingHurtEvent;
 import org.jetbrains.annotations.Nullable;
 import radon.jujutsu_kaisen.JujutsuKaisen;
 import radon.jujutsu_kaisen.ability.JJKAbilities;
@@ -55,61 +58,55 @@ public class QuickDraw extends Ability implements Ability.IToggled {
         return ActivationType.TOGGLED;
     }
 
-    private static void attack(LivingEntity owner, Entity entity) {
-        if (entity instanceof AbstractArrow || entity instanceof ThrowableItemProjectile) {
-            owner.lookAt(EntityAnchorArgument.Anchor.EYES, entity.position().add(0.0D, entity.getBbHeight() / 2.0F, 0.0D));
-            owner.swing(InteractionHand.MAIN_HAND, true);
-            entity.discard();
-        } else if (entity instanceof LivingEntity) {
-            if (entity.invulnerableTime > 0) return;
+    private static void attack(LivingEntity owner, LivingEntity entity) {
+        if (entity.invulnerableTime > 0) return;
 
-            owner.lookAt(EntityAnchorArgument.Anchor.EYES, entity.position().add(0.0D, entity.getBbHeight() / 2.0F, 0.0D));
+        owner.lookAt(EntityAnchorArgument.Anchor.EYES, entity.position().add(0.0D, entity.getBbHeight() / 2.0F, 0.0D));
 
-            IJujutsuCapability cap = owner.getCapability(JujutsuCapabilityHandler.INSTANCE);
+        IJujutsuCapability cap = owner.getCapability(JujutsuCapabilityHandler.INSTANCE);
 
-            if (cap == null) return;
+        if (cap == null) return;
 
-            IAbilityData data = cap.getAbilityData();
+        IAbilityData data = cap.getAbilityData();
 
-            for (int i = 0; i < Barrage.DURATION; i++) {
-                boolean last = i == Barrage.DURATION - 1;
+        for (int i = 0; i < Barrage.DURATION; i++) {
+            boolean last = i == Barrage.DURATION - 1;
 
-                data.delayTickEvent(() -> {
-                    owner.swing(InteractionHand.MAIN_HAND, true);
+            data.delayTickEvent(() -> {
+                owner.swing(InteractionHand.MAIN_HAND, true);
 
-                    Vec3 look = RotationUtil.getTargetAdjustedLookAngle(owner);
+                Vec3 look = RotationUtil.getTargetAdjustedLookAngle(owner);
 
-                    for (int j = 0; j < 4; j++) {
-                        Vec3 pos = owner.getEyePosition().add(look.scale(2.5D));
-                        ((ServerLevel) owner.level()).sendParticles(owner.getItemInHand(InteractionHand.MAIN_HAND).getItem() instanceof SwordItem ? ParticleTypes.SWEEP_ATTACK : ParticleTypes.CLOUD,
-                                pos.x + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
-                                pos.y + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
-                                pos.z + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
-                                0, 0.0D, 0.0D, 0.0D, 1.0D);
-                    }
-                    for (int j = 0; j < 4; j++) {
-                        Vec3 pos = owner.getEyePosition().add(look.scale(2.5D));
-                        ((ServerLevel) owner.level()).sendParticles(ParticleTypes.CRIT,
-                                pos.x + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
-                                pos.y + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
-                                pos.z + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
-                                0, 0.0D, 0.0D, 0.0D, 1.0D);
-                    }
+                for (int j = 0; j < 4; j++) {
+                    Vec3 pos = owner.getEyePosition().add(look.scale(2.5D));
+                    ((ServerLevel) owner.level()).sendParticles(owner.getItemInHand(InteractionHand.MAIN_HAND).getItem() instanceof SwordItem ? ParticleTypes.SWEEP_ATTACK : ParticleTypes.CLOUD,
+                            pos.x + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
+                            pos.y + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
+                            pos.z + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
+                            0, 0.0D, 0.0D, 0.0D, 1.0D);
+                }
+                for (int j = 0; j < 4; j++) {
+                    Vec3 pos = owner.getEyePosition().add(look.scale(2.5D));
+                    ((ServerLevel) owner.level()).sendParticles(ParticleTypes.CRIT,
+                            pos.x + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
+                            pos.y + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
+                            pos.z + (HelperMethods.RANDOM.nextDouble() - 0.5D) * 2.5D,
+                            0, 0.0D, 0.0D, 0.0D, 1.0D);
+                }
 
-                    Vec3 pos = owner.getEyePosition().add(look);
-                    owner.level().playSound(null, pos.x, pos.y, pos.z, SoundEvents.GENERIC_SMALL_FALL, SoundSource.MASTER, 1.0F, 0.3F);
+                Vec3 pos = owner.getEyePosition().add(look);
+                owner.level().playSound(null, pos.x, pos.y, pos.z, SoundEvents.GENERIC_SMALL_FALL, SoundSource.MASTER, 1.0F, 0.3F);
 
-                    if (owner instanceof Player player) {
-                        player.attack(entity);
-                    } else {
-                        owner.doHurtTarget(entity);
-                    }
+                if (owner instanceof Player player) {
+                    player.attack(entity);
+                } else {
+                    owner.doHurtTarget(entity);
+                }
 
-                    if (!last) {
-                        entity.invulnerableTime = 0;
-                    }
-                }, i * 2);
-            }
+                if (!last) {
+                    entity.invulnerableTime = 0;
+                }
+            }, i * 2);
         }
     }
 
@@ -131,8 +128,8 @@ public class QuickDraw extends Ability implements Ability.IToggled {
 
             if (domain == null) return;
 
-            for (Entity entity : owner.level().getEntities(owner, domain.getBoundingBox())) {
-                if (entity == domain || entity.distanceTo(domain) > domain.getRadius()) continue;
+            for (LivingEntity entity : EntityUtil.getTouchableEntities(LivingEntity.class, owner.level(), owner, domain.getBoundingBox())) {
+                if (entity.distanceTo(domain) > domain.getRadius()) continue;
 
                 attack(owner, entity);
             }
@@ -188,7 +185,7 @@ public class QuickDraw extends Ability implements Ability.IToggled {
     @Mod.EventBusSubscriber(modid = JujutsuKaisen.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
     public static class ForgeEvents {
         @SubscribeEvent
-        public static void onLivingAttack(LivingAttackEvent event) {
+        public static void onLivingHurt(LivingHurtEvent event) {
             LivingEntity victim = event.getEntity();
 
             if (victim.level().isClientSide) return;
@@ -202,9 +199,35 @@ public class QuickDraw extends Ability implements Ability.IToggled {
             if (!data.hasToggled(JJKAbilities.QUICK_DRAW.get()) ||
                     !data.hasToggled(JJKAbilities.FALLING_BLOSSOM_EMOTION.get())) return;
 
-            Entity attacker = event.getSource().getDirectEntity();
+            if (!(event.getSource().getDirectEntity() instanceof Projectile)) return;
 
-            QuickDraw.attack(victim, attacker);
+            ItemStack stack = victim.getItemInHand(InteractionHand.MAIN_HAND);
+
+            if (!(stack.getItem() instanceof SwordItem)) return;
+
+            victim.swing(InteractionHand.MAIN_HAND, true);
+
+            int amount = Math.round(event.getAmount());
+            int remaining = stack.getMaxDamage() - stack.getDamageValue();
+
+            int blocked = Math.min(remaining, amount);
+
+            stack.hurtAndBreak(blocked, victim, entity -> {
+                entity.broadcastBreakEvent(InteractionHand.MAIN_HAND);
+
+                if (victim instanceof Player player) {
+                    EventHooks.onPlayerDestroyItem(player, stack, InteractionHand.MAIN_HAND);
+                }
+                entity.stopUsingItem();
+            });
+
+            float reduced = amount - blocked;
+
+            event.setAmount(reduced);
+
+            if (reduced == 0.0F) {
+                victim.hurtMarked = false;
+            }
         }
     }
 }
