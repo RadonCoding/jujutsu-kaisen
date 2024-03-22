1bf7f166608b9f31356a9437cfdb0821a4f98878
RadonCoding <henry.welch@reborn.com>
2024-03-22 15:28:00 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/ability/misc/VeilDeactivate.java
+++ src/main/java/radon/jujutsu_kaisen/ability/misc/VeilDeactivate.java
@@ -2,6 +2,7 @@ package radon.jujutsu_kaisen.ability.misc;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.player.Player;
@@ -13,6 +14,7 @@ import org.jetbrains.annotations.Nullable;
 import radon.jujutsu_kaisen.JujutsuKaisen;
 import radon.jujutsu_kaisen.ability.MenuType;
 import radon.jujutsu_kaisen.ability.base.Ability;
+import radon.jujutsu_kaisen.block.entity.VeilBlockEntity;
 import radon.jujutsu_kaisen.block.entity.VeilRodBlockEntity;
 import radon.jujutsu_kaisen.data.capability.IJujutsuCapability;
 import radon.jujutsu_kaisen.data.capability.JujutsuCapabilityHandler;
@@ -44,13 +46,17 @@ public class VeilDeactivate extends Ability {
 
         ISorcererData data = cap.getSorcererData();
 
-        if (owner.level().isClientSide) return;
+        if (!(owner.level() instanceof ServerLevel level)) return;
 
         HitResult hit = RotationUtil.getHitResult(owner, owner.getEyePosition(),
                 owner.getEyePosition().add(owner.getLookAngle().scale(VeilActivate.RANGE)), target -> !target.isSpectator());
 
-        if (!(hit instanceof EntityHitResult entityHit)) return;
-        if (!(entityHit.getEntity() instanceof VeilEntity veil)) return;
+        if (!(hit instanceof BlockHitResult blockHit)) return;
+
+        BlockPos pos = blockHit.getBlockPos();
+
+        if (!(owner.level().getBlockEntity(pos) instanceof VeilBlockEntity be)) return;
+        if (!(level.getEntity(be.getParentUUID()) instanceof VeilEntity veil)) return;
         if (veil.getOwner() != owner) return;
 
         data.removeSummon(veil);
