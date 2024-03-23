a267e5bdf0ed16830a310907d5e10a0802aa9033
RadonCoding <henry.welch@reborn.com>
2024-03-23 23:09:43 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/event/JJKEventHandler.java
+++ src/main/java/radon/jujutsu_kaisen/event/JJKEventHandler.java
@@ -158,8 +158,6 @@ public class JJKEventHandler {
                 }
             }
 
-            if (source.is(DamageTypeTags.BYPASSES_RESISTANCE)) return;
-
             IJujutsuCapability cap = victim.getCapability(JujutsuCapabilityHandler.INSTANCE);
 
             if (cap == null) return;
