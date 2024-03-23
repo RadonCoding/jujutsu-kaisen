8b45be39eb570dd3647a6d74a7b960a558f18a56
RadonCoding <henry.welch@reborn.com>
2024-03-23 17:18:27 +0200

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/client/render/entity/SelfEmbodimentOfPerfectionRenderer.java
+++ src/main/java/radon/jujutsu_kaisen/client/render/entity/SelfEmbodimentOfPerfectionRenderer.java
@@ -21,10 +21,8 @@ public class SelfEmbodimentOfPerfectionRenderer extends GeoEntityRenderer<SelfEm
     @Override
     public void preRender(PoseStack poseStack, SelfEmbodimentOfPerfectionEntity animatable, BakedGeoModel model, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
         float yaw = Mth.lerp(partialTick, animatable.yRotO, animatable.getYRot());
-        float pitch = Mth.lerp(partialTick, animatable.xRotO, animatable.getXRot());
 
         poseStack.mulPose(Axis.YP.rotationDegrees(yaw));
-        poseStack.mulPose(Axis.XN.rotationDegrees(pitch));
 
         super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
     }
