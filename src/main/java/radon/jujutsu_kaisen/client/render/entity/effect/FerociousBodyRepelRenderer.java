b3a174cd0ceedfd5479f934927c6e54d931f4def
RadonCoding <henry.welch@reborn.com>
2024-03-31 19:04:04 +0300

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/client/render/entity/effect/FerociousBodyRepelRenderer.java
+++ src/main/java/radon/jujutsu_kaisen/client/render/entity/effect/FerociousBodyRepelRenderer.java
@@ -7,12 +7,17 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import radon.jujutsu_kaisen.JujutsuKaisen;
 import radon.jujutsu_kaisen.entity.effect.BodyRepelEntity;
 import radon.jujutsu_kaisen.entity.effect.FerociousBodyRepelEntity;
+import radon.jujutsu_kaisen.entity.projectile.EelGrappleProjectile;
 import radon.jujutsu_kaisen.entity.ten_shadows.DivineDogEntity;
 import software.bernie.geckolib.cache.object.BakedGeoModel;
+import software.bernie.geckolib.cache.object.GeoBone;
 import software.bernie.geckolib.model.DefaultedEntityGeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
@@ -23,13 +28,31 @@ public class FerociousBodyRepelRenderer extends GeoEntityRenderer<FerociousBodyR
 
     @Override
     public void preRender(PoseStack poseStack, FerociousBodyRepelEntity animatable, BakedGeoModel model, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-        float yaw = Mth.lerp(partialTick, animatable.yRotO, animatable.getYRot());
-        float pitch = Mth.lerp(partialTick, animatable.xRotO, animatable.getXRot());
+        if (!(animatable.getOwner() instanceof LivingEntity owner)) return;
 
-        poseStack.mulPose(Axis.YN.rotationDegrees(yaw));
-        poseStack.mulPose(Axis.XP.rotationDegrees(pitch));
+        poseStack.translate(0.0F, animatable.getBbHeight() / 2.0F, 0.0F);
+
+        Vec3 ownerPos = getPosition(owner, owner.getBbHeight() / 2.0F, partialTick);
+        Vec3 projectilePos = getPosition(animatable, animatable.getBbHeight() / 2.0F, partialTick);
+        Vec3 relative = ownerPos.subtract(projectilePos);
+        float f0 = (float) relative.length();
+        relative = relative.normalize();
+        float f1 = (float) Math.acos(relative.y);
+        float f2 = (float) Math.atan2(relative.z, relative.x);
+        poseStack.mulPose(Axis.YP.rotationDegrees((((Mth.PI / 2.0F) - f2) * (180.0F / Mth.PI)) - 180.0F));
+        poseStack.mulPose(Axis.XN.rotationDegrees((f1 * (180.0F / Mth.PI)) - 90.0F));
+
+        GeoBone tail = this.model.getBone("tail").orElseThrow();
+        tail.setScaleZ(f0);
 
         super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
+    }
+
+    private static Vec3 getPosition(Entity entity, double yOffset, float pPartialTick) {
+        double d0 = entity.xOld + (entity.getX() - entity.xOld) * (double) pPartialTick;
+        double d1 = yOffset + entity.yOld + (entity.getY() - entity.yOld) * (double) pPartialTick;
+        double d2 = entity.zOld + (entity.getZ() - entity.zOld) * (double) pPartialTick;
+        return new Vec3(d0, d1, d2);
     }
 
     @Override
