f3ae80fbe2a9cb64ebbf00ef00a3ffba5bd14f39
RadonCoding <henry.welch@reborn.com>
2024-06-06 22:39:04 +0300

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/client/render/entity/effect/PureLoveBeamRenderer.java
+++ src/main/java/radon/jujutsu_kaisen/client/render/entity/effect/PureLoveBeamRenderer.java
@@ -31,59 +31,6 @@ public class PureLoveBeamRenderer extends EntityRenderer<PureLoveBeamEntity> {
     @Override
     public void render(PureLoveBeamEntity pEntity, float pEntityYaw, float pPartialTick, @NotNull PoseStack pPoseStack, @NotNull MultiBufferSource pBuffer, int pPackedLight) {
         if (pEntity.getTime() < pEntity.getCharge()) return;
-        
-
-    }
-
-    private void renderBeam(PureLoveBeamEntity entity, PoseStack poseStack, MultiBufferSource source, float partialTicks, float width, float length, boolean inner, boolean glowSecondPass) {
-        poseStack.pushPose();
-
-        float v = ((float) entity.tickCount + partialTicks) * -0.25F;
-        float v1 = v + length * (inner ? 0.5F : 0.15F);
-        float f4 = -width;
-        float f5 = 0;
-        float f6 = 0.0F;
-        PoseStack.Pose posestack$pose = poseStack.last();
-        Matrix4f matrix4f = posestack$pose.pose();
-        VertexConsumer vertexconsumer = source.getBuffer(JJKRenderTypes.glow(new ResourceLocation(JujutsuKaisen.MOD_ID, "textures/entity/bem.png")));
-        for (int j = 0; j <= 4; ++j) {
-            Matrix3f matrix3f = posestack$pose.normal();
-            float f7 = Mth.cos((float) Math.PI + (float) j * ((float) Math.PI * 2F) / (float) 4) * width;
-            float f8 = Mth.sin((float) Math.PI + (float) j * ((float) Math.PI * 2F) / (float) 4) * width;
-            float f9 = (float) j + 1;
-            vertexconsumer.vertex(matrix4f, f4 * 0.55F, f5 * 0.55F, 0.0F)
-                    .color(255, 255, 255, 255)
-                    .uv(f6, v)
-                    .overlayCoords(OverlayTexture.NO_OVERLAY)
-                    .uv2(LightTexture.FULL_BRIGHT)
-                    .normal(posestack$pose, 0.0F, -1.0F, 0.0F)
-                    .endVertex();
-            vertexconsumer.vertex(matrix4f, f4, f5, length)
-                    .color(255, 255, 255, 255)
-                    .uv(f6, v1)
-                    .overlayCoords(OverlayTexture.NO_OVERLAY)
-                    .uv2(LightTexture.FULL_BRIGHT)
-                    .normal(posestack$pose, 0.0F, -1F, 0.0F)
-                    .endVertex();
-            vertexconsumer.vertex(matrix4f, f7, f8, length)
-                    .color(255, 255, 255, 255)
-                    .uv(f9, v1)
-                    .overlayCoords(OverlayTexture.NO_OVERLAY)
-                    .uv2(LightTexture.FULL_BRIGHT)
-                    .normal(posestack$pose, 0.0F, -1F, 0.0F)
-                    .endVertex();
-            vertexconsumer.vertex(matrix4f, f7 * 0.55F, f8 * 0.55F, 0.0F)
-                    .color(255, 255, 255, 255)
-                    .uv(f9, v)
-                    .overlayCoords(OverlayTexture.NO_OVERLAY)
-                    .uv2(LightTexture.FULL_BRIGHT)
-                    .normal(posestack$pose, 0.0F, -1.0F, 0.0F)
-                    .endVertex();
-            f4 = f7;
-            f5 = f8;
-            f6 = f9;
-        }
-        poseStack.popPose();
     }
 
     @Override
