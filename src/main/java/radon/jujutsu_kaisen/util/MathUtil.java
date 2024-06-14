347c53829cca7d1556c0e2251eff70513a10d576
RadonCoding <henry.welch@reborn.com>
2024-06-14 12:17:09 +0300

    Read CHANGES.md

--- src/main/java/radon/jujutsu_kaisen/util/MathUtil.java
+++ src/main/java/radon/jujutsu_kaisen/util/MathUtil.java
@@ -25,30 +25,28 @@ public class MathUtil {
 
                 det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
 
-        if (det == 0) {
-            throw new ArithmeticException("Matrix is singular and cannot be inverted.");
-        }
+        if (det == 0) throw new ArithmeticException("Matrix is singular and cannot be inverted.");
 
-        float invDet = 1.0F / det;
+        det = 1.0F / det;
 
-        Matrix4f result = new Matrix4f();
-        result.m00((a11 * b11 - a12 * b10 + a13 * b09) * invDet);
-        result.m01((a02 * b10 - a01 * b11 - a03 * b09) * invDet);
-        result.m02((a31 * b05 - a32 * b04 + a33 * b03) * invDet);
-        result.m03((a22 * b04 - a21 * b05 - a23 * b03) * invDet);
-        result.m10((a12 * b08 - a10 * b11 - a13 * b07) * invDet);
-        result.m11((a00 * b11 - a02 * b08 + a03 * b07) * invDet);
-        result.m12((a32 * b02 - a30 * b05 - a33 * b01) * invDet);
-        result.m13((a20 * b05 - a22 * b02 + a23 * b01) * invDet);
-        result.m20((a10 * b10 - a11 * b08 + a13 * b06) * invDet);
-        result.m21((a01 * b08 - a00 * b10 - a03 * b06) * invDet);
-        result.m22((a30 * b04 - a31 * b02 + a33 * b00) * invDet);
-        result.m23((a21 * b02 - a20 * b04 - a23 * b00) * invDet);
-        result.m30((a11 * b07 - a10 * b09 - a12 * b06) * invDet);
-        result.m31((a00 * b09 - a01 * b07 + a02 * b06) * invDet);
-        result.m32((a31 * b01 - a30 * b03 - a32 * b00) * invDet);
-        result.m33((a20 * b03 - a21 * b01 + a22 * b00) * invDet);
+        Matrix4f inverse = new Matrix4f();
+        inverse.m00((a11 * b11 - a12 * b10 + a13 * b09) * det);
+        inverse.m01((a02 * b10 - a01 * b11 - a03 * b09) * det);
+        inverse.m02((a31 * b05 - a32 * b04 + a33 * b03) * det);
+        inverse.m03((a22 * b04 - a21 * b05 - a23 * b03) * det);
+        inverse.m10((a12 * b08 - a10 * b11 - a13 * b07) * det);
+        inverse.m11((a00 * b11 - a02 * b08 + a03 * b07) * det);
+        inverse.m12((a32 * b02 - a30 * b05 - a33 * b01) * det);
+        inverse.m13((a20 * b05 - a22 * b02 + a23 * b01) * det);
+        inverse.m20((a10 * b10 - a11 * b08 + a13 * b06) * det);
+        inverse.m21((a01 * b08 - a00 * b10 - a03 * b06) * det);
+        inverse.m22((a30 * b04 - a31 * b02 + a33 * b00) * det);
+        inverse.m23((a21 * b02 - a20 * b04 - a23 * b00) * det);
+        inverse.m30((a11 * b07 - a10 * b09 - a12 * b06) * det);
+        inverse.m31((a00 * b09 - a01 * b07 + a02 * b06) * det);
+        inverse.m32((a31 * b01 - a30 * b03 - a32 * b00) * det);
+        inverse.m33((a20 * b03 - a21 * b01 + a22 * b00) * det);
 
-        return result;
+        return inverse;
     }
 }
